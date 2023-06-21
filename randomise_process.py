dfa03fce0b0ce48715a13be88fb12437da917986
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-06-21 22:19:13 +0100

    Update randomise_process.py

--- randomise_process.py
+++ randomise_process.py
@@ -1,21 +1,18 @@
 import sqlite3
 import gc
 gc.disable()
-def inserting(games, conn, c):
+def inserting(games, conn):
     for game in games:
-        result = game[1]
-        moves = game[2]
-        print(result)
-        print(moves)
-        try:
-            c.execute("INSERT INTO games (result, moves) VALUES (?,?)",
-                    (result, moves))
-        except sqlite3.IntegrityError:
-            pass
+        for param in game:
+            result = param[1]
+            moves = param[2]
+            try:
+                conn.execute("INSERT INTO games (result, moves) VALUES (?,?)",
+                        (result, moves))
+            except sqlite3.IntegrityError:
+                pass
 
     conn.commit()
-    del conn
-    del c
     del games
     gc.enable()
     gc.collect()
