f9f61442ad1fff9fb63328e02d30b6393f6bc20f
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-06-24 17:54:41 +0100

    Update aiucieval.py

--- aiucieval.py
+++ aiucieval.py
@@ -1,36 +1,41 @@
-# zan1ling4 | 真零 | (pronounced Jun Ling)
 # imports
 import numpy as np
 import torch
 import torch.nn as nn
 import chess
-import torch.nn.init as init
+import torch.nn.init as init 
 # set up AI (Sequential NN)
 
 
 class Tanh200(nn.Module):
+    def __init__(self):
+        super(Tanh200, self).__init__()
+
     def forward(self, x):
         return torch.tanh(x / 200)
+
 
 class Agent(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(833, 512)
-        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc1 = nn.Linear(833, 2048)
+        self.bn1 = nn.BatchNorm1d(2048)
+        self.dropout1 = nn.Dropout(p=0.45)
         self.relu = nn.LeakyReLU(0.05)
-        self.layer2 = nn.Linear(512, 1)
-        self.dropout2 = nn.Dropout(p=0.25)
+        self.layer2 = nn.Linear(2048, 1)
+        self.dropout2 = nn.Dropout(p=0.45)
         self.tanh200 = Tanh200()
         self.hidden_layers = nn.ModuleList()
 
-        # Initialize weights of Linear layers
-        init.uniform_(self.fc1.weight, -1, 1)
-        init.uniform_(self.layer2.weight, -1, 1)
-        
+        # Initialize weights of Linear layers using Xavier initialization
+        init.xavier_uniform_(self.fc1.weight)
+        init.xavier_uniform_(self.layer2.weight)
+
         self.loss = nn.MSELoss()
 
     def forward(self, x):
         x = self.fc1(x)
+        x = self.bn1(x)
         x = self.dropout1(x)
         x = self.relu(x)
         x = self.layer2(x)
@@ -40,10 +45,9 @@ class Agent(nn.Module):
 
 
 model = Agent()
-weights_path = "./zlv6.pt"
-state_dict = torch.load(weights_path)
+weights_path = "./zlv7_pro.pt"
+state_dict = torch.load(weights_path, map_location=torch.device("cpu"))
 model.load_state_dict(state_dict)
-
 
 # set up important functions
 # board_data - this function will turn a python-chess board into a matrix for the AI to evaluate
@@ -72,41 +76,40 @@ def static_eval(board):
     return score
 
 
-def negamax_ab_iterative(board, alpha, beta, colour: int, max_depth: int):
-    best_move = None
-    for depth in range(1, max_depth + 1):
-        value, move = negamax_ab(board, alpha, beta, colour, depth)
-        if best_move is None or value > best_move[0]:
-            best_move = (value, move)
-    return best_move
-
-
-def negamax_ab(board, alpha, beta, colour: int, depth: int):
+def negamax_ab(board, alpha, beta, colour, model, depth=2):
     if depth == 0 or board.is_game_over():  # check if the depth is 0 or "terminal node"
         if colour == 1:
             move_turn = 0  # my eval accepts 0 for white and black for 1 :/
         else:
             move_turn = 1
+        # game after one hot encoding
         matrix_game = np.array([board_data(board)])
-        matrix_game = np.concatenate((matrix_game, np.array([[move_turn]])), axis=1)
+        matrix_game = np.concatenate(
+            (matrix_game, np.array([[move_turn]])), axis=1
+        )  # have to append the move turn - the AI needs to know this
         matrix_game = torch.tensor(matrix_game, dtype=torch.float32)
-        score = model(matrix_game)
+        model.eval()
+        score = model(
+            matrix_game
+        )  # EVALUTATION - high score for winning (if white/black wins, high score, vice versa)
         score = float(score)
+        if board.is_game_over():
+            return score * colour * (1 if board.turn == chess.WHITE else -1)
         return score * colour
+
     child_nodes = list(board.legal_moves)
     # child_nodes = order_moves(child_nodes) # make an ordermove function
-    value = -np.inf
+    best_score = -np.inf
     for child in child_nodes:
         board.push(child)  # Push the current child move on the board
-        v = negamax_ab(board, -beta, -alpha, -colour, depth - 1)
-        value = max(value, -v)
+        score = -negamax_ab(board, -beta, -alpha, -colour, model, depth - 1)
         board.pop()  # Pop the current child move from the board
-        alpha = max(alpha, value)
+
+        best_score = max(best_score, score)
+        alpha = max(alpha, best_score)
         if alpha >= beta:
             break
-
-    return value
-
+    return best_score
 
 # set up chess game
 
@@ -130,15 +133,23 @@ def analyse_move_nodes(board, ready, max_nodes):
             if n_count < max_nodes:
                 board.push(move)
                 move_score = negamax_ab(
-                    board, -np.inf, np.inf, colour, depth=2
-                )  # NOTE:TEMPORARY
+                    board, -np.inf, np.inf, -colour, 2)
                 m_dict[str(move)] = move_score
-                m_dict = {
-                    k: v
-                    for k, v in sorted(
-                        m_dict.items(), key=lambda item: item[1], reverse=False
-                    )
-                }  # reverse=True to find best move with highest score
+                if colour == -1:
+                    m_dict = {
+                        k: v
+                        for k, v in sorted(
+                            m_dict.items(), key=lambda item: item[1], reverse=False
+                        )  # reverse=False to find the best move with highest score
+                    }
+                else:
+                    m_dict = {
+                        k: v
+                        for k, v in sorted(
+                            m_dict.items(), key=lambda item: item[1], reverse=True
+                        )  # reverse=False to find the best move with highest score
+                    }
+                best_move = list(m_dict.keys())[0]  # best move, first key
                 board.pop()
                 n_count += 1
         m = iter(m_dict)
@@ -160,7 +171,7 @@ def analyse_move(board, ready, depth=2):
         m_dict = {}
         for move in legal_moves:
             board.push(move)
-            move_score = negamax_ab(board, -np.inf, np.inf, colour, depth)
+            move_score = negamax_ab(board, -np.inf, np.inf, colour, model,  depth)
             m_dict[str(move)] = move_score
             board.pop()
         m_dict = {
