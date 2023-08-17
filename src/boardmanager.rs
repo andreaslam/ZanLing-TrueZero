907a5117f3db6b5d79d1a248ed31ee9feeba2289
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-17 20:57:20 +0100

    Update boardmanager.rs

--- src/boardmanager.rs
+++ src/boardmanager.rs
@@ -12,11 +12,11 @@ impl BoardStack {
         let target_hash = self.board.hash();
         let mut is_over = false;
         let is_found = self.move_stack.contains(&target_hash);
-        if !is_found { // reset
+        if !is_found {
             self.move_stack.push(target_hash); // add hash into stack
         } else { // repetition found!
             self.move_stack.push(target_hash);
-            if let count = self.move_stack.iter().filter(|&x| *x == target_hash).count(); >= 3 { // draw by repetition!
+            if self.move_stack.iter().filter(|&x| *x == target_hash).count() >= 3 { // draw by repetition!
                 is_over = true;
             }
         }
