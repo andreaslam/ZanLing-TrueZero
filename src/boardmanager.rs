3e26eb57d15e38097fb1b81e13669ca0ca57ab8d
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-17 20:54:17 +0100

    Create boardmanager.rs

---
+++ src/boardmanager.rs
@@ -0,0 +1,26 @@
+use cozy_chess::*;
+
+struct BoardStack {
+    board: Board,
+    move_stack: Vec<u64>,
+
+}
+
+impl BoardStack {
+
+    fn compare(&self) -> bool {
+        let target_hash = self.board.hash();
+        let mut is_over = false;
+        let is_found = self.move_stack.contains(&target_hash);
+        if !is_found { // reset
+            self.move_stack.push(target_hash); // add hash into stack
+        } else { // repetition found!
+            self.move_stack.push(target_hash);
+            if let count = self.move_stack.iter().filter(|&x| *x == target_hash).count(); >= 3 { // draw by repetition!
+                is_over = true;
+            }
+        }
+    is_over
+    }
+
+}
