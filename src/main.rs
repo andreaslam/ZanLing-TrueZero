7d0926bd6de4489ed8f27bf15d266bae7c92f3bb
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-18 13:34:33 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use cozy_chess::*;
+// use tch::*;
 mod decoder;
 mod mcts_trainer;
-use tch::*;
+mod boardmanager;
+mod mvs;
+use crate::boardmanager::BoardStack;
+use crate::decoder::eval_board;
+
 // training loop code
 fn main() {
-    
+    let board = Board::default();
+    let bs = BoardStack {board:board,move_stack:Vec::new()};
+    eval_board(board, bs);
 }
