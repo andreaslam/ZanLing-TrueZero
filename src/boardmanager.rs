d0eb61faace9457bb357f779e7483e20b2090d6a
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-18 09:58:57 +0100

    Update boardmanager.rs

--- src/boardmanager.rs
+++ src/boardmanager.rs
@@ -1,5 +1,5 @@
 use cozy_chess::*;
-
+use std::collections::HashMap;
 struct BoardStack {
     board: Board,
     move_stack: Vec<u64>,
@@ -8,7 +8,7 @@ struct BoardStack {
 
 impl BoardStack {
 
-    fn compare(&self) -> bool {
+    pub fn compare(&mut self) -> bool {
         let target_hash = self.board.hash();
         let mut is_over = false;
         let is_found = self.move_stack.contains(&target_hash);
@@ -21,6 +21,26 @@ impl BoardStack {
             }
         }
     is_over
+    }
+    // get number of repetitions for decoder.rs
+
+    pub fn get_reps(&self) -> u8 {
+        let mut count_map: HashMap<u64, usize> = HashMap::new();
+        for &num in &self.move_stack {
+            *count_map.entry(num).or_insert(0) += 1;
+        }
+        let max_repetitions = count_map.values().cloned().max().unwrap_or(0);
+        max_repetitions as u8
+    }
+
+    pub fn play(&mut self, mv:&str) {
+        let is_over_draws = self.compare();
+        let status = self.board.status();
+        let is_board_over = status != GameStatus::Ongoing;
+        while !is_board_over | !is_over_draws {
+            // convert move to playable ones and play it
+            self.board.play(mv.parse().unwrap());
+        } 
     }
 
 }
