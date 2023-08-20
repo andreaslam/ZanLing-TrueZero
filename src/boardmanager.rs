f2133ad261bc6d556266cb548901e628385843ff
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-20 22:36:04 +0100

    Update boardmanager.rs

--- src/boardmanager.rs
+++ src/boardmanager.rs
@@ -1,8 +1,8 @@
 use cozy_chess::*;
 
 pub struct BoardStack {
-    board: Board,
-    move_stack: Vec<u64>,
+    pub board: Board,
+    pub move_stack: Vec<u64>,
 
 }
 
@@ -22,23 +22,26 @@ impl BoardStack {
         }
     is_over
     }
-    
+
+    fn count_occurrences(&self,vec: &Vec<u64>, target: u64) -> usize {
+        vec.iter().filter(|&&x| x == target).count()
+    }
     // get number of repetitions for decoder.rs
     // remember to push current position BEFORE calling on get_reps
     pub fn get_reps(&self) -> u8 {
         // reps only for the current position, not the global maximum of repetitions recorded
-        let max_repetitions = self.move_stack.iter().filter(|&&x| x == self.move_stack[self.move_stack.len()]).count();
+        let max_repetitions = self.count_occurrences(&self.move_stack, self.move_stack[self.move_stack.len()-1]);
         max_repetitions as u8
     }
 
     // play function to be called in selfplay.rs
-    pub fn play(&mut self, mv:&str) {
+    pub fn play(&mut self, mv:&Move) {
         let is_over_draws = self.compare();
         let status = self.board.status();
         let is_board_over = status != GameStatus::Ongoing;
-        while !is_board_over | !is_over_draws {
+        if !is_board_over | !is_over_draws {
             // convert move to playable ones and play it
-            self.board.play(mv.parse().unwrap());
+            self.board.play(*mv);
         } 
     }
 
