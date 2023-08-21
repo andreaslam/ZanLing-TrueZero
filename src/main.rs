65942f11f21c37914400628bac0a857d9080bf16
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-22 00:12:52 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,30 @@
-use boardmanager::BoardStack;
-use selfplay::DataGen;
 use cozy_chess::*;
+use tz_rust::boardmanager::BoardStack;
+use tz_rust::selfplay::DataGen;
 // use tch::*;
-mod decoder;
-mod mcts_trainer;
-mod boardmanager;
-mod mvs;
-mod selfplay;
-mod dirichlet;
+// mod decoder;
+// mod mcts_trainer;
+// mod boardmanager;
+// mod mvs;
+// mod selfplay;
+// mod dirichlet;
 
 use std::env;
 // training loop code
 fn main() {
-
-
     env::set_var("RUST_BACKTRACE", "0");
-    let stack:Vec<u64> = Vec::new();
     let board = Board::default();
-    let bs = BoardStack{
-        board,
-        move_stack:stack
+    let bs = BoardStack::new(board);
 
-    };
-    let mut dg = DataGen{
+    let mut dg = DataGen {
         iterations: 10,
-        stack_manager: bs
+        bs,
     };
-    
-    let (training_data,pi_list,move_idx_list,results_list) = dg.generate_batch();
 
-    println!("{:?}, {:?}, {:?}, {:?}", training_data, pi_list, move_idx_list, results_list);
+    let (training_data, pi_list, move_idx_list, results_list) = dg.generate_batch();
 
+    println!(
+        "{:?}, {:?}, {:?}, {:?}",
+        training_data, pi_list, move_idx_list, results_list
+    );
 }
