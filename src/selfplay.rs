bec6dbbc5e44a2945ffe76df3878216b50439b18
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-22 00:13:03 +0100

    Update selfplay.rs

--- src/selfplay.rs
+++ src/selfplay.rs
@@ -1,46 +1,43 @@
-use crate::mcts_trainer::get_move;
 use crate::boardmanager::BoardStack;
+use crate::mcts_trainer::get_move;
 use cozy_chess::*;
-// use tch::Tensor;
+use tch::Tensor;
 
 // selfplay code
 
 pub struct DataGen {
     pub iterations: u32, // number of games needed per batch of training data
-    pub stack_manager: BoardStack, 
+    pub bs: BoardStack,
 }
 
 impl DataGen {
-    fn play_game(&mut self) -> (Vec<Vec<f32>>, Vec<f32>, Option<Vec<usize>>, i8) {
-        
-        let mut memory: Vec<Vec<f32>> = Vec::new();
-        let mut pi: Vec<f32> = Vec::new();
-        let mut move_idx:Option<Vec<usize>> = None;
-        let mut is_over = self.stack_manager.compare();
-        while !is_over {
-            memory = Vec::new();
-            let mv: cozy_chess::Move;
-            let memory_piece: Vec<f32>;
-            (mv, memory_piece, pi, move_idx) = get_move(&self.stack_manager.board, &self);// memory_piece is the input data used for training
-            println!("{:?}", mv);
-            memory.push(memory_piece);
-            self.stack_manager.play(&mv);
-            is_over = self.stack_manager.compare();
+    fn play_game(&mut self) -> (Vec<BoardStack>, Vec<Vec<f32>>, Vec<Vec<usize>>, Option<cozy_chess::Color>) {
+        let mut memory: Vec<BoardStack> = Vec::new();
+        let mut pi: Vec<Vec<f32>> = Vec::new();
+        let mut move_idx: Vec<Vec<usize>> = Vec::new();
+        while self.bs.status() == GameStatus::Ongoing {
+            let (mv, pi_piece, move_idx_piece) = get_move(&self.bs);
+            memory.push(self.bs.clone());
+            pi.push(pi_piece);
+            move_idx.push(move_idx_piece);
+            self.bs.play(mv);
         }
-        let o = self.stack_manager.board.side_to_move();
-        let outcome: i8;
-        match o {
-            Color::White => {
-                outcome = 1;
-            }
-            Color::Black => {
-                outcome = -1;
-            }
-        }
-        (memory,pi,move_idx, outcome)
+        let outcome: Option<Color> = match self.bs.status() {
+            GameStatus::Drawn => None,
+            GameStatus::Won => Some(!self.bs.board().side_to_move()),
+            GameStatus::Ongoing => panic!(),
+        };
+        (memory, pi, move_idx, outcome)
     }
 
-    pub fn generate_batch(&mut self) -> (Vec<Vec<Vec<f32>>>, Vec<Vec<f32>>, Vec<Option<Vec<usize>>>, Vec<i8>) {
+    pub fn generate_batch(
+        &mut self,
+    ) -> (
+        Vec<Vec<Tensor>>,
+        Vec<Vec<f32>>,
+        Vec<Option<Vec<usize>>>,
+        Vec<i8>,
+    ) {
         let training_data = Vec::new();
         let pi_list = Vec::new();
         let move_idx_list = Vec::new();
@@ -50,13 +47,12 @@ impl DataGen {
             let mut pi_list = Vec::new();
             let mut move_idx_list = Vec::new();
             let mut results_list = Vec::new();
-            let (memory_piece_game,pi,move_idx, z) = self.play_game(); // z is final game result
+            let (memory_piece_game, pi, move_idx, z) = self.play_game(); // z is final game result
             training_data.push(memory_piece_game);
             pi_list.push(pi);
             move_idx_list.push(move_idx);
             results_list.push(z); // can't append training data and results tgt :/
         }
-        (training_data,pi_list,move_idx_list,results_list)
+        (training_data, pi_list, move_idx_list, results_list)
     }
-
 }
