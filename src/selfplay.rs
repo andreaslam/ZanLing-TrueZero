ace71f33f484c9ae1be5cb89adb770127602036b
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-08-30 22:42:22 +0100

    Update selfplay.rs

--- src/selfplay.rs
+++ src/selfplay.rs
@@ -1,31 +1,46 @@
 use crate::boardmanager::BoardStack;
 use crate::mcts_trainer::get_move;
-use cozy_chess::*;
+use cozy_chess::{Board, Color, GameStatus, Move};
 use tch::Tensor;
 
 // selfplay code
-
+#[derive(PartialEq, Clone, Debug)]
 pub struct DataGen {
     pub iterations: u32, // number of games needed per batch of training data
-    pub bs: BoardStack,
 }
 
 impl DataGen {
-    fn play_game(&mut self) -> (Vec<BoardStack>, Vec<Vec<f32>>, Vec<Vec<usize>>, Option<cozy_chess::Color>) {
+    fn play_game(
+        &mut self,
+    ) -> (
+        Vec<BoardStack>,
+        Vec<Vec<f32>>,
+        Vec<Vec<usize>>,
+        Option<cozy_chess::Color>,
+    ) {
         let mut memory: Vec<BoardStack> = Vec::new();
         let mut pi: Vec<Vec<f32>> = Vec::new();
         let mut move_idx: Vec<Vec<usize>> = Vec::new();
-        while self.bs.status() == GameStatus::Ongoing {
-            let (mv, pi_piece, move_idx_piece) = get_move(&self.bs);
-            memory.push(self.bs.clone());
+        let mut counter = 0;
+        let mut tau = 1;
+        let board = Board::default();
+        let mut bs = BoardStack::new(board);
+
+        while bs.status() == GameStatus::Ongoing {
+            if counter == 30 {
+                tau = 0
+            }
+            let (mv, pi_piece, move_idx_piece) = get_move(bs.clone());
+            memory.push(bs.clone());
             pi.push(pi_piece);
-            move_idx.push(move_idx_piece);
-            self.bs.play(mv);
+            move_idx.push(move_idx_piece.unwrap());
+            bs.play(mv);
+            counter += 1;
         }
-        let outcome: Option<Color> = match self.bs.status() {
+        let outcome: Option<Color> = match bs.status() {
             GameStatus::Drawn => None,
-            GameStatus::Won => Some(!self.bs.board().side_to_move()),
-            GameStatus::Ongoing => panic!(),
+            GameStatus::Won => Some(!bs.board().side_to_move()),
+            GameStatus::Ongoing => panic!("Game is still ongoing!"),
         };
         (memory, pi, move_idx, outcome)
     }
@@ -33,26 +48,24 @@ impl DataGen {
     pub fn generate_batch(
         &mut self,
     ) -> (
-        Vec<Vec<Tensor>>,
-        Vec<Vec<f32>>,
-        Vec<Option<Vec<usize>>>,
-        Vec<i8>,
+        Vec<Vec<BoardStack>>,
+        Vec<Vec<Vec<f32>>>,
+        Vec<Vec<Vec<usize>>>,
+        Vec<Option<Color>>,
     ) {
-        let training_data = Vec::new();
-        let pi_list = Vec::new();
-        let move_idx_list = Vec::new();
-        let results_list = Vec::new();
+        let mut pi_list: Vec<Vec<Vec<f32>>> = Vec::new();
+        let mut move_idx_list = Vec::new();
+        let mut results_list: Vec<Option<Color>> = Vec::new(); // None for draw
+        let mut boardstack_list: Vec<Vec<BoardStack>> = Vec::new();
         for _ in 0..self.iterations {
-            let mut training_data = Vec::new();
-            let mut pi_list = Vec::new();
-            let mut move_idx_list = Vec::new();
-            let mut results_list = Vec::new();
-            let (memory_piece_game, pi, move_idx, z) = self.play_game(); // z is final game result
-            training_data.push(memory_piece_game);
+            let (memory, pi, move_idx, outcome) = self.play_game(); // z is final game result
+                                                                    // memory in the form of boardstack
+            println!("{:?}", outcome);
+            boardstack_list.push(memory);
             pi_list.push(pi);
             move_idx_list.push(move_idx);
-            results_list.push(z); // can't append training data and results tgt :/
+            results_list.push(outcome); // raw outcome of who won, not accounting for POV yet
         }
-        (training_data, pi_list, move_idx_list, results_list)
+        (boardstack_list, pi_list, move_idx_list, results_list)
     }
 }
