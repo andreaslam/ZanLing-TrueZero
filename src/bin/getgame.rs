025011ce000974bf780ac6a6dd0c581ceb642035
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-10-07 18:43:09 +0100

    Update getgame.rs

--- src/bin/getgame.rs
+++ src/bin/getgame.rs
@@ -7,5 +7,5 @@ fn main() {
     // test game with MCTS
     env::set_var("RUST_BACKTRACE", "1");
     let mut dg = DataGen { iterations: 1 };
-    let (_, _, _, _) = dg.generate_batch();
+    _ = dg.generate_batch();
 }
