95a0f94f24bc50e856164dde0d2d4639bfa57828
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-10-07 18:42:55 +0100

    Update getdecode.rs

--- src/bin/getdecode.rs
+++ src/bin/getdecode.rs
@@ -1,5 +1,5 @@
 use std::{convert, env};
-
+use stopwatch::Stopwatch;
 use cozy_chess::Board;
 use tz_rust::{boardmanager::BoardStack, decoder::convert_board, decoder::eval_board};
 
@@ -12,7 +12,11 @@ fn main() {
     )
     .unwrap();
     let bs = BoardStack::new(board);
+    let mut sw = Stopwatch::new();
+    sw.start();
     let converted_tensor = convert_board(&bs);
+    sw.stop();
     let print_vec: Vec<f32> = Vec::try_from(converted_tensor).expect("Error");
     println!("{:?}", print_vec);
+    println!("Elapsed time: {}ms", sw.elapsed_ms());
 }
