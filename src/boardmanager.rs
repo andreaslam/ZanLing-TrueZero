1d204e1415f897499e9b68d7ab0f0ea09a33af92
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-10-08 10:03:45 +0100

    Update boardmanager.rs

--- src/boardmanager.rs
+++ src/boardmanager.rs
@@ -1,251 +1,68 @@
-use crate::{
-    boardmanager::BoardStack,
-    mcts_trainer::{Net, Node, Tree},
-    mvs::get_contents,
-};
-use cozy_chess::{Color, Move, Piece, Rank, Square};
-use std::vec;
-use tch::{IValue, Kind, Tensor};
+use cozy_chess::*;
 
-pub fn eval_state(board: Tensor, net: &Net) -> anyhow::Result<(Tensor, Tensor)> {
-    let model = &net.net;
-    // model.set_eval(); // set to eval!
-    // model.to(net.device, Kind::Float, true);
-    // reshape the model (originally from 1D)
-    let b = board;
-    let b = b.unsqueeze(0);
-    let b = b.reshape([-1, 21, 8, 8]);
-    // b.print();
-    let b: Tensor = b.to(net.device);
-    let board = IValue::Tensor(b);
-    let output = model.forward_is(&[board])?;
-    let output_tensor = match output {
-        IValue::Tuple(b) => b,
-        a => panic!("the output is not a TensorList {:?}", a),
-    };
-    let (board_eval, policy) = (&output_tensor[0], &output_tensor[1]);
-    let board_eval = match board_eval {
-        IValue::Tensor(b) => b,
-        a => panic!("the output is not a TensorList {:?}", a),
-    };
-    let policy = match policy {
-        IValue::Tensor(b) => b,
-        a => panic!("the output is not a TensorList {:?}", a),
-    };
-    Ok((board_eval.clone(board_eval), policy.clone(policy)))
+#[derive(PartialEq, Clone, Debug)]
+pub struct BoardStack {
+    board: Board,
+    move_stack: Vec<u64>,
+    status: GameStatus,
 }
 
-pub fn convert_board(bs: &BoardStack) -> Tensor {
-    // FULL LIST HERE:
-    // sq1 - white's turn
-    // sq2 - black's turn
-    // sq3, sq4 - castling pos l + r (us)
-    // sq5, sq6 - castling pos l + r (opponent)
-    // sql7, sql8 -  sqs for bits for the repetition counter
-    // sq9 - sq20 - sqs for turn to move + non-turn to move's pieces
-    // sq21 - en passant square if any
-
-    // sq1 - white's turn
-    // sq2 - black's turn
-
-    // it seems that creating a Vec, processing everything first is faster than doing Tensor::zeros() and then stacking them
-    // so i instead work with Vecs, get all of them together and convert them into a single Tensor at the end
-
-    let us = bs.board().side_to_move();
-    let mut scalar_data = vec![0.0; 8];
-    if bs.board().side_to_move() == Color::Black {
-        scalar_data[1] = 1.0;
-    } else {
-        scalar_data[0] = 1.0;
-    }
-
-    let li;
-    if us == Color::White {
-        // can't use Colour::ALL since the order of Colour::ALL is always going to be [white, black]
-        li = [Color::White, Color::Black];
-    } else {
-        li = [Color::Black, Color::White];
-    }
-
-    let mut c = 2;
-
-    for color in li {
-        let l_rights = bs.board().castle_rights(color).long;
-        let s_rights = bs.board().castle_rights(color).short;
-
-        scalar_data[c] = if l_rights.is_some() { 1.0 } else { 0.0 };
-        scalar_data[c + 1] = if s_rights.is_some() { 1.0 } else { 0.0 };
-        c += 2
-    }
-
-    scalar_data[6] = bs.get_reps() as f32;
-    scalar_data[7] = bs.board().halfmove_clock() as f32;
-
-    // flatten to 1d
-
-    let mut counter = 0;
-    let mut pieces_sqs: Vec<f32> = vec![0.0; 64 * 12];
-    for colour in li {
-        for piece in Piece::ALL {
-            for tile in bs.board().colored_pieces(colour, piece) {
-                if li[0] == Color::Black {
-                    pieces_sqs[(63 - (tile.rank() as usize * 8 + (7 - tile.file() as usize)))
-                        + (64 * counter)] = 1.0;
-                } else {
-                    pieces_sqs[(tile.rank() as usize * 8 + tile.file() as usize) + 64 * counter] =
-                        1.0;
-                }
-            }
-            counter += 1
+impl BoardStack {
+    pub fn new(board: Board) -> Self {
+        Self {
+            status: board.status(),
+            board,
+            move_stack: Vec::new(),
         }
     }
 
-    let is_ep = bs.board().en_passant();
-    // let fenstr = format!("{}", bs.board());
-    // println!("    board FEN: {}", fenstr);
-    // println!("En passant status: {:?}", is_ep);
-    let mut sq21: Vec<f32> = vec![0.0; 64];
-    match is_ep {
-        Some(is_ep) => {
-            if us == Color::White {
-                // 4 for white and 5 for black for victim
-                let row = Rank::Fourth;
-                let ep_sq = Square::new(is_ep, row);
-                sq21[ep_sq.rank() as usize * 8 + ep_sq.file() as usize] = 1.0;
-            } else {
-                let row = Rank::Fifth;
-                let ep_sq = Square::new(is_ep, row);
-                sq21[63 - (ep_sq.rank() as usize * 8 + (7 - ep_sq.file() as usize))] = 1.0;
-            }
-        }
-        None => {}
-    };
+    // get number of repetitions for decoder.rs
 
-    let mut all_data: Vec<f32> = Vec::new();
-
-    for v in &scalar_data {
-        for _ in 0..64 {
-            all_data.push(*v);
-        }
+    pub fn get_reps(&self) -> usize {
+        // reps only for the current position, not the global maximum of repetitions recorded
+        let target = self.board.hash();
+        (&self.move_stack).iter().filter(|&x| *x == target).count()
     }
 
-    all_data.extend(pieces_sqs);
-    all_data.extend(sq21);
-
-    let all_data = Tensor::from_slice(&all_data);
-    all_data // all_data is 1d
-}
-
-pub fn eval_board(
-    bs: &BoardStack,
-    net: &Net,
-    tree: &mut Tree,
-    selected_node_idx: &usize,
-) -> Vec<usize> {
-    let contents = get_contents();
-    let b = convert_board(bs);
-
-    let output = eval_state(b, &net).expect("Error");
-
-    let (board_eval, policy) = output; // check policy, eval ordering!
-
-    let board_eval = board_eval.squeeze();
-
-    let board_eval: Vec<f32> = Vec::try_from(board_eval).expect("Error");
-
-    let board_eval = Tensor::from_slice(&vec![board_eval[0]]);
-
-    let value = Tensor::tanh(&board_eval);
-
-    let policy = policy.squeeze();
-    let policy: Vec<f32> = Vec::try_from(policy).expect("Error");
-    let value = f32::try_from(value).expect("Error");
-
-    let value = match bs.board().side_to_move() {
-        Color::Black => -value,
-        Color::White => value,
-    };
-
-    // step 1 - get the corresponding idx for legal moves
-
-    let mut legal_moves: Vec<Move> = Vec::new();
-    bs.board().generate_moves(|moves| {
-        // Unpack dense move set into move list
-        legal_moves.extend(moves);
-        false
-    });
-
-    let mut fm: Vec<Move> = Vec::new();
-    if bs.board().side_to_move() == Color::Black {
-        // flip move
-        for mv in &legal_moves {
-            fm.push(Move {
-                from: mv.from.flip_rank(),
-                to: mv.to.flip_rank(),
-                promotion: mv.promotion,
-            })
-        }
-    } else {
-        fm = legal_moves.clone();
-    }
-
-    legal_moves = fm;
-
-    let mut idx_li: Vec<usize> = Vec::new();
-
-    for mov in &legal_moves {
-        // let mov = format!("{}", mov);
-        if let Some(idx) = contents.iter().position(|x| mov == x) {
-            idx_li.push(idx as usize);
-        }
-    }
-
-    // step 2 - using the idx in step 1, index all the policies involved
-    let mut pol_list: Vec<f32> = Vec::new();
-    for id in &idx_li {
-        pol_list.push(policy[*id]);
-    }
-
-    // println!("{:?}", pol_list);
-
-    // step 3 - softmax
-
-    let sm = Tensor::from_slice(&pol_list);
-
-    let sm = Tensor::softmax(&sm, 0, Kind::Float);
-
-    let pol_list: Vec<f32> = Vec::try_from(sm).expect("Error");
-
-    // println!("{:?}", pol_list);
-
-    // println!("        V={}", &value);
-
-    // step 4 - iteratively append nodes into class
-    let mut counter = 0;
-    tree.nodes[*selected_node_idx].eval_score = value;
-    // tree.nodes[*selected_node_idx].eval_score = 0.0;
-    let ct = tree.nodes.len();
-    for (mv, pol) in legal_moves.iter().zip(pol_list.iter()) {
-        // println!("VAL {}", value);
-        let fm: Move;
-        if bs.board().side_to_move() == Color::Black {
-            // flip move
-            fm = Move {
-                from: mv.from.flip_rank(),
-                to: mv.to.flip_rank(),
-                promotion: mv.promotion,
-            };
+    // play function to be called instead of calling directly on the cozy-chess board
+    pub fn play(&mut self, mv: Move) {
+        assert!(self.status == GameStatus::Ongoing); // check if prev board is valid (can play a move)
+        self.move_stack.push(self.board.hash());
+        self.board.play(mv);
+        let is_all_gone = self.board.occupied().len() == 2;
+        let is_sure_draw = self.board.occupied().len() <= 3
+            && (self.board.pieces(Piece::Bishop).len() == 1
+                || self.board.pieces(Piece::Knight).len() == 1); // (K or K+N or K+B) vs (K or K+N or K+B)
+        self.status = if self.get_reps() == 2
+            || self.board.halfmove_clock() == 100
+            || is_all_gone
+            || is_sure_draw
+        {
+            GameStatus::Drawn
         } else {
-            fm = *mv;
-        }
-        // FLAT POLICY VER
-        // let child = Node::new(1.0/legal_moves.len() as f32, Some(*selected_node_idx), Some(fm));
-        let child = Node::new(*pol, Some(*selected_node_idx), Some(fm));
-        // println!("{:?}, {:?}, {:?}", mv, child.policy, child.eval_score);
-        tree.nodes.push(child); // push child to the tree Vec<Node>
-        counter += 1
+            self.board.status()
+        };
     }
-    tree.nodes[*selected_node_idx].children = ct..ct + counter; // push numbers
-                                                                // println!("{:?}", tree.nodes.len());
-    idx_li
+
+    pub fn is_terminal(&self) -> bool {
+        let status = self.status();
+        status != GameStatus::Ongoing // returns true if game is over (not ongoing)
+    }
+
+    pub fn board(&self) -> &Board {
+        &self.board
+    }
+
+    pub fn status(&self) -> GameStatus {
+        let is_all_gone = self.board.occupied().len() == 2;
+        let is_sure_draw = self.board.occupied().len() <= 3
+            && (self.board.pieces(Piece::Bishop).len() == 1
+                || self.board.pieces(Piece::Knight).len() == 1); // (K or K+N or K+B) vs (K or K+N or K+B)
+        if self.get_reps() == 2 || self.board.halfmove_clock() == 100 || is_all_gone || is_sure_draw
+        {
+            GameStatus::Drawn
+        } else {
+            self.board.status()
+        }
+    }
 }
