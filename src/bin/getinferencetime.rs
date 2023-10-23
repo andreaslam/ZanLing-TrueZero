f981ff3e603d3be2df0a17f13fe3d1e9445197b6
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-10-23 12:16:37 +0100

    Update getinferencetime.rs

--- src/bin/getinferencetime.rs
+++ src/bin/getinferencetime.rs
@@ -6,13 +6,19 @@ use tch::Tensor;
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     let net = Net::new();
-    let data = Tensor::from_slice(&[0.0 as f32;1344*2048]); // 8*8*21 = 1344
-    let mut sw = Stopwatch::new();
-    println!("{:?}", net.device);
-    sw.start();
-    (_, _) = eval_state(data, &net).expect("Error");   
-    sw.stop();
-    println!("Elapsed time: {}ms", sw.elapsed_ms());
-    let eps = 1.0 / (sw.elapsed_ms() as f32 / 1000.0);
-    println!("Evaluations per second: {}", eps);
+    let mut avg = Vec::new();
+    const batch_size: usize = 128;
+    for _ in 0..100 {
+        let mut sw = Stopwatch::new();
+        let data = Tensor::from_slice(&[0.0 as f32;1344*batch_size]); // 8*8*21 = 1344
+        sw.start();
+        (_, _) = eval_state(data, &net).expect("Error");   
+        sw.stop();
+        println!("Elapsed time: {}ms", sw.elapsed_ms());
+        let eps = batch_size.clone() as f32/ (sw.elapsed_ms() as f32 / 1000.0);
+        println!("Evaluations per second: {}", eps);
+        avg.push(eps);
+    }
+    println!("Average evaluations per second: {}", avg.iter().sum::<f32>() as f32 / avg.len() as f32);
+    println!("{:?}", avg);
 }
