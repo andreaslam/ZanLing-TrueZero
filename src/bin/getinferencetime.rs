ccb4ba6a88eba57ca4b68d83747d440ce8dc5d4d
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-10-23 12:23:23 +0100

    Update getinferencetime.rs

--- src/bin/getinferencetime.rs
+++ src/bin/getinferencetime.rs
@@ -1,34 +1,38 @@
 use std::env;
 use stopwatch::Stopwatch;
-use tz_rust::{decoder::eval_state, mcts_trainer::Net};
 use tch::Tensor;
+use tz_rust::{decoder::eval_state, mcts_trainer::Net};
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     let net = Net::new();
     let mut avg = Vec::new();
-    const BATCH_SIZE: usize = 2048;
+    const BATCH_SIZE: usize = 128;
 
-    // warmup loop 
+    // warmup loop
 
     for _ in 0..10 {
-        let data = Tensor::from_slice(&[0.0 as f32;1344*BATCH_SIZE]); // 8*8*21 = 1344
-        (_, _) = eval_state(data, &net).expect("Error");   
+        let data = Tensor::from_slice(&[0.0 as f32; 1344 * BATCH_SIZE]); // 8*8*21 = 1344
+        (_, _) = eval_state(data, &net).expect("Error");
     }
 
     // timed, benchmarked loop
 
     for _ in 0..100 {
         let mut sw = Stopwatch::new();
-        let data = Tensor::from_slice(&[0.0 as f32;1344*BATCH_SIZE]); // 8*8*21 = 1344
+        let data = Tensor::from_slice(&[0.0 as f32; 1344 * BATCH_SIZE]); // 8*8*21 = 1344
         sw.start();
-        (_, _) = eval_state(data, &net).expect("Error");   
+        (_, _) = eval_state(data, &net).expect("Error");
         sw.stop();
         println!("Elapsed time: {}ms", sw.elapsed_ms());
-        let eps = BATCH_SIZE.clone() as f32/ (sw.elapsed_ms() as f32 / 1000.0);
-        println!("Evaluations per second: {}", eps);
+        let eps = BATCH_SIZE.clone() as f32 / (sw.elapsed_ms() as f32 / 1000.0);
+        println!("Evaluations per second: {} evals/s", eps);
         avg.push(eps);
     }
-    println!("Average evaluations per second: {} (batch size {})", avg.iter().sum::<f32>() as f32 / avg.len() as f32, BATCH_SIZE);
+    println!(
+        "Average evaluations per second: {} evals/s (batch size {})",
+        avg.iter().sum::<f32>() as f32 / avg.len() as f32,
+        BATCH_SIZE
+    );
     println!("{:?}", avg);
 }
