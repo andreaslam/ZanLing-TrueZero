69db2426da2e8ccb82bd84ea37e33ba6bd9ab5d3
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-11-11 10:38:14 +0000

    Update selfplay.rs

--- src/selfplay.rs
+++ src/selfplay.rs
@@ -1,9 +1,14 @@
-use crate::dataformat::Position;
-use crate::mcts_trainer::get_move;
-use crate::{boardmanager::BoardStack, dataformat::Simulation};
+use crate::{
+    boardmanager::BoardStack,
+    dataformat::{Position, Simulation},
+    executor::{Message, Packet},
+    mcts_trainer::{get_move, MAX_NODES},
+};
 use cozy_chess::{Board, GameStatus, Move};
+use flume::{Receiver, Sender};
 use rand::prelude::*;
 use rand_distr::WeightedIndex;
+use std::time::Instant;
 // selfplay code
 
 #[derive(PartialEq, Clone, Debug, Copy)]
@@ -13,15 +18,23 @@ pub struct DataGen {
 }
 
 impl DataGen {
-    pub fn play_game(&self) -> Simulation {
+    pub fn play_game(&self, tensor_exe_send: Sender<Packet>) -> Simulation {
         let mut bs = BoardStack::new(Board::default());
         // let mut value: Vec<f32> = Vec::new();
         let mut positions: Vec<Position> = Vec::new();
+        let thread_name = std::thread::current()
+            .name()
+            .unwrap_or("unnamed")
+            .to_owned();
         while bs.status() == GameStatus::Ongoing {
-            let (mv, v_p, move_idx_piece, search_data, visits) = get_move(bs.clone());
+            let sw = Instant::now();
+            let (mv, v_p, move_idx_piece, search_data, visits) =
+                get_move(bs.clone(), tensor_exe_send.clone());
+            let elapsed = sw.elapsed().as_secs();
             let final_mv = if positions.len() > 30 {
                 // when tau is "infinitesimally small", pick the best move
-                println!("{:#}", mv);
+                let nps = MAX_NODES as u64 / elapsed as u64;
+                println!("thread {}, {:#}, {}nps", thread_name, mv, nps);
                 mv
             } else {
                 let weighted_index = WeightedIndex::new(&search_data.policy).unwrap();
@@ -34,7 +47,8 @@ impl DataGen {
                     legal_moves.extend(moves);
                     false
                 });
-                println!("{:#}", legal_moves[sampled_idx]);
+                let nps = MAX_NODES as u64 / elapsed;
+                println!("thread {}, {:#}, {}nps", thread_name, mv, nps);
                 legal_moves[sampled_idx]
             };
 
@@ -65,8 +79,8 @@ impl DataGen {
     pub fn generate_batch(&mut self) -> Vec<Simulation> {
         let mut sims: Vec<Simulation> = Vec::new();
         while sims.len() < self.iterations as usize {
-            let sim = self.play_game();
-            sims.push(sim);
+            // let sim = self.play_game();
+            // sims.push(sim);
         }
         sims
     }
