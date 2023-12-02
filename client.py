7b78e1b6cf3acaa208485d5bf98668703e586c02
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-02 14:59:26 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import socket
 from enum import Enum
-# import threading
-# import queue
+from lib.games import Game
 from trainingloop import train, load_file
-import torch
-from torchrl.data import ReplayBuffer, ListStorage 
+from lib.loop import LoopBuffer
+from lib.logger import Logger
+
 
 # message enums
 class MessageSend(Enum):  # message to send to rust
     NEW_NETWORK = "newnet"
     STOP_SERVER = "stop"
+
 
 class Server:
     def __init__(self, host, port):
@@ -22,7 +23,7 @@ class Server:
         while not connected:
             try:
                 self.socket.connect((self.host, self.port))
-                print("Connected to server...")
+                print("Connected to server!.")
                 connected = True
             except ConnectionRefusedError as e:
                 print(f"Connection failed: {e}. Retrying...")
@@ -50,23 +51,49 @@ server.send("python-training")
 
 # define file buffer
 
-rb = ReplayBuffer(
-    storage=ListStorage(max_size=1000),
-    batch_size=5,
+
+logged_in = False
+
+
+BUFFER_SIZE = 1000
+BATCH_SIZE = 200
+loopbuf = LoopBuffer(
+    Game.find("chess"), target_positions=BUFFER_SIZE, test_fraction=0.2
 )
 while True:
+    log = Logger()
+    log.start_batch()
     received_data = server.receive()
     print(f"Received: {received_data}")
-    if "new-training-data" in received_data:
-        # append file buffer
-        
-        file_path = received_data.split()[1].strip()      
-        load_file(file_path)
 
-# Close the server connection outside the loop
+    if "python-training" in received_data:  # connected to server, got confirmation
+        logged_in = True
+        print("logged in")
+
+    if "new-training-data" in received_data and logged_in:
+        # append file buffer
+
+        file_path = received_data.split()[1].strip()
+        data = load_file(file_path)
+        loopbuf.append(log, data)
+
+        if loopbuf.position_count >= 1000:
+            sample = loopbuf.sampler(
+                batch_size=BATCH_SIZE,
+                unroll_steps=None,
+                include_final=False,
+                random_symmetries=False,
+                only_last_gen=False,
+                test=True,
+            )
+            batch = sample.next_batch()
+            train(batch)
+            loopbuf = LoopBuffer(
+                Game.find("chess"), target_positions=BUFFER_SIZE, test_fraction=0.2
+            )
+
+    # Close the server connection outside the loop
     if received_data == "shutdown":
-         
         server.close()
         print("Connection closed.")
-    
-    
+        break
