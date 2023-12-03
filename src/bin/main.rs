258fa142aba1e2a157fe17d43f07ed7d02787372
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-03 00:04:08 +0000

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 use crossbeam::thread;
-use flume::{Receiver, Sender};
+use flume::{Receiver, RecvError, Selector, Sender};
 use std::{
     env,
     io::{Read, Write},
@@ -26,32 +26,8 @@ fn main() {
         .write_all("rust-datagen".as_bytes())
         .expect("Failed to send data");
     println!("Connected to server!");
-    let mut buffer = [0; 16384];
-    let id: String = loop {
-        match stream.read(&mut buffer) {
-            Ok(recv_net) if recv_net == 0 => {
-                // no more data, connection closed by server
-                println!("Server closed the connection");
-                // Force quit the program when the server closes the connection
-                std::process::exit(0);
-            }
-            Ok(_) => {
-                let msg = String::from_utf8_lossy(&buffer[..]); // convert received bytes to String
-                if msg.starts_with("rust-datagen") {
-                    let segment: Vec<String> = msg.split_whitespace().map(String::from).collect();
-                    break segment[1].clone();
-                }
-            }
-            Err(err) => {
-                panic!("Error reading from server: {}", err);
-            }
-        };
-    };
-
-    let id = id.chars().filter(|&c| c != '\0').collect(); // remove invalid chars
-
     let (game_sender, game_receiver) = flume::bounded::<CollectorMessage>(1);
-    let num_threads = 5;
+    let num_threads = 1;
     let num_executors = 1;
     thread::scope(|s| {
         let mut selfplay_masters: Vec<DataGen> = Vec::new();
@@ -66,36 +42,24 @@ fn main() {
             vec_communicate_exe_recv.push(communicate_exe_recv);
         }
 
+        // send-recv pair between commander and collector
+
+        let (id_send, id_recv) = flume::bounded::<String>(1);
+
         s.builder()
             .name("commander".to_string())
             .spawn(|_| {
                 commander_main(
                     vec_communicate_exe_send,
                     &mut stream.try_clone().expect("clone failed"),
+                    id_send,
                 )
             })
             .unwrap();
-
-        // s.spawn(move |_| {
-        //     commander_main(communicate_exe_send);
-        // });
         // selfplay threads
-        // let mut exe_resenders: Vec<Sender<Message>> = Vec::new(); // sent FROM executor to mcts
-        // let mut tensor_senders: Vec<Receiver<Tensor>> = Vec::new(); // sent FROM mcts to executor
         let (tensor_exe_send, tensor_exe_recv) = flume::bounded::<Packet>(1); // mcts to executor
-                                                                              // let (eval_exe_send, eval_exe_recv) = flume::bounded::<Message>(1); // executor to mcts
-                                                                              // let mut exe_count = 0;
         for n in 0..num_threads {
             // // executor
-            // if n % num_executors == 0 {
-            //     let communicate_exe_recv_clone = communicate_exe_recv.clone();
-            //     let tensor_exe_recv_clone = tensor_exe_recv.clone();
-            //     s.builder()
-            //     .name(format!("executor_{}", exe_count.to_string()))
-            //     .spawn(move |_| executor_main(communicate_exe_recv_clone, tensor_exe_recv_clone, num_threads))
-            //     .unwrap();
-            //     exe_count += 1;
-            // }
             // sender-receiver pair to communicate for each thread instance to the executor
             let sender_clone = game_sender.clone();
             let mut selfplay_master = DataGen { iterations: 1 };
@@ -112,11 +76,6 @@ fn main() {
                     )
                 })
                 .unwrap();
-            // exe_resenders.push(eval_exe_send);
-            // tensor_senders.push(tensor_exe_recv);
-            // s.spawn(move |_| {
-            //     generator_main(&sender_clone, &mut selfplay_master);
-            // });
 
             selfplay_masters.push(selfplay_master.clone());
         }
@@ -128,20 +87,15 @@ fn main() {
                 collector_main(
                     &game_receiver,
                     &mut stream.try_clone().expect("clone failed"),
-                    id,
+                    id_recv,
                 )
             })
             .unwrap();
-
-        // s.spawn(move |_| {
-        //     collector_main(&game_receiver);
-        // });
         // executor
         let mut n = 0;
         for communicate_exe_recv in vec_communicate_exe_recv {
             // send/recv pair between executor and commander
             let eval_per_sec_sender = game_sender.clone();
-            // let communicate_exe_recv_clone = communicate_exe_recv.clone();
             let tensor_exe_recv_clone = tensor_exe_recv.clone();
             s.builder()
                 .name(format!("executor_{}", n.to_string()))
@@ -156,10 +110,6 @@ fn main() {
                 .unwrap();
             n += 1;
         }
-
-        // s.spawn(move |_| {
-        //     executor_main(communicate_exe_recv);
-        // });
     })
     .unwrap();
 }
@@ -181,14 +131,17 @@ fn generator_main(
 fn collector_main(
     receiver: &Receiver<CollectorMessage>,
     server_handle: &mut TcpStream,
-    id: String,
+    id_recv: Receiver<String>,
 ) {
     let mut counter = 0;
+    let id = id_recv.recv().unwrap();
+
     let mut path = format!("generator_{}_games_{}", id, counter);
     let mut bin_output = BinaryOutput::new(path.clone(), "chess").unwrap();
     let mut nps_start_time = Instant::now();
-    let mut evals_start_time = Instant::now();
     let mut nps_vec: Vec<f32> = Vec::new();
+    let mut evals_start_time = Instant::now();
+    let mut evals_vec: Vec<f32> = Vec::new();
 
     loop {
         let msg = receiver.recv().unwrap();
@@ -217,46 +170,90 @@ fn collector_main(
             }
             CollectorMessage::ExecutorStatistics(evals_per_sec) => {
                 if evals_start_time.elapsed() >= Duration::from_secs(1) {
-                    let nps: f32 = nps_vec.iter().sum();
+                    let nps: f32 = evals_vec.iter().sum();
                     // println!("{} evals/s", nps);
                     evals_start_time = Instant::now();
-                    nps_vec = Vec::new();
+                    evals_vec = Vec::new();
                 } else {
-                    nps_vec.push(evals_per_sec);
+                    evals_vec.push(evals_per_sec);
                 }
             }
         }
     }
 }
-fn commander_main(vec_exe_sender: Vec<Sender<String>>, server_handle: &mut TcpStream) {
-    let mut curr_net = String::new(); // changed to String type
+fn commander_main(
+    vec_exe_sender: Vec<Sender<String>>,
+    server_handle: &mut TcpStream,
+    id_sender: Sender<String>,
+) {
+    let mut curr_net = String::new();
     let mut buffer = [0; 16384];
+    let mut is_initialised = false;
+    let mut net_path = String::new(); // initialize net_path with an empty string
+
     loop {
-        let recv_net = match server_handle.read(&mut buffer) {
-            Ok(recv_net) if recv_net == 0 => {
+        println!("is_initialised: {} ", is_initialised);
+        println!("net_path: {} ", net_path);
+        let recv_msg = match server_handle.read(&mut buffer) {
+            Ok(msg) if msg == 0 => {
                 // no more data, connection closed by server
                 println!("Server closed the connection");
-                // Force quit the program when the server closes the connection
+                // force quit the program when the server closes the connection
                 std::process::exit(0);
             }
-            Ok(recv_net) => recv_net,
+            Ok(id) if !is_initialised => {
+                let msg = String::from_utf8_lossy(&buffer[..]); // convert received bytes to String
+                if msg.starts_with("rust-datagen") {
+                    println!("obtained id: {}", id);
+                    id // Explicitly return the value
+                } else {
+                    continue;
+                }
+            }
+
+            Ok(recv_net) => {
+                let msg = String::from_utf8_lossy(&buffer[..]); // convert received bytes to String
+                if msg.starts_with("newnet") {
+                    println!("found net path: {}", recv_net);
+                    recv_net // Explicitly return the value
+                } else {
+                    continue;
+                }
+            }
+
             Err(err) => {
                 eprintln!("Error reading from server: {}", err);
                 break;
             }
         };
 
-        let net_path = String::from_utf8_lossy(&buffer[..recv_net]).to_string(); // convert received bytes to String
+        if is_initialised {
+            net_path = String::from_utf8_lossy(&buffer[..recv_msg]).to_string();
+            let segment: Vec<String> = net_path.split_whitespace().map(String::from).collect();
+            net_path = segment[1].clone();
+            net_path.retain(|c| c != '\0'); // remove invalid chars
+        } else {
+            let id = String::from_utf8_lossy(&buffer[..recv_msg]).to_string(); // convert received bytes to String
+            let segment: Vec<String> = id.split_whitespace().map(String::from).collect();
+            let mut id = segment[1].clone();
+            id.retain(|c| c != '\0'); // remove invalid chars
+                                      // send to collector
+
+            let _ = id_sender.send(id).unwrap();
+
+            is_initialised = true;
+            println!("INITIALISED");
+        }
 
         if curr_net != net_path {
             for exe_sender in &vec_exe_sender {
                 exe_sender
-                    .send("chess_16x128_gen3634.pt".to_string())
+                    .send(net_path.clone())
                     .unwrap();
                 // println!("SENT!");
             }
 
-            curr_net = net_path;
+            curr_net = net_path.clone();
         }
 
         buffer = [0; 16384];
