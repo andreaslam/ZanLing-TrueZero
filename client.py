a1e79fc679a0cbb95aacc40e526ee8a83ee9a8ba
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-09 10:50:42 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,24 +1,25 @@
 import socket
 from enum import Enum
 from lib.games import Game
-from trainingloop import train, load_file
+from trainingloop import load_file, trainer_loop
 from lib.loop import LoopBuffer
 from lib.logger import Logger
+import threading
+from queue import Queue
 
 
-# message enums
-class MessageSend(Enum):  # message to send to rust
+class MessageSend(Enum):
     NEW_NETWORK = "newnet"
     STOP_SERVER = "stop"
 
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
+    def connect(self) -> None:
         connected = False
         while not connected:
             try:
@@ -29,63 +30,58 @@ class Server:
                 print(f"Connection failed: {e}. Retrying...")
                 continue
 
-    def send(self, message):
+    def send(self, message: str) -> None:
         self.socket.sendall(message.encode())
 
-    def receive(self):
-        data = self.socket.recv(16384)
-        return data.decode()
+    def receive(self) -> str:
+        data_received = self.socket.recv(16384)
+        return data_received.decode()
 
-    def close(self):
+    def close(self) -> None:
         self.socket.close()
 
 
+# Constants
 HOST = "127.0.0.1"
 PORT = 8080
+BUFFER_SIZE = 1000
+BATCH_SIZE = 200
 
+# Initialization
 server = Server(HOST, PORT)
 server.connect()
 
-# identification - this is python training
 server.send("python-training")
 
-# define file buffer
-
-
-logged_in = False
-
-
-BUFFER_SIZE = 1000
-BATCH_SIZE = 200
 loopbuf = LoopBuffer(
     Game.find("chess"), target_positions=BUFFER_SIZE, test_fraction=0.2
 )
+log = Logger()
+log.start_batch()
+
+logged_in = False
+communication_queue = Queue()
+
+thread = threading.Thread(target=trainer_loop, args=(communication_queue,))
+thread.start()
+
 while True:
-    log = Logger()
-    log.start_batch()
     received_data = server.receive()
     print(f"Received: {received_data}")
 
-    if "python-training" in received_data:  # connected to server, got confirmation
-        logged_in = True
-        print("logged in")
-        # send neural net
-        server.send("newnet: chess_16x128_gen3634.pt ")  # hardcode for now
+    if (
+        "python-training" in received_data
+        or "rust-datagen" in received_data
+        or "requesting-net" in received_data
+    ):
+        server.send("newnet: chess_16x128_gen3634.pt")
 
-    if "rust-datagen" in received_data:
-        server.send("newnet: chess_16x128_gen3634.pt ")  # hardcode for now
-
-    if "requesting-net" in received_data:
-        server.send("newnet: chess_16x128_gen3634.pt ")  # hardcode for now
-    
     if "new-training-data" in received_data and logged_in:
-        # append file buffer
-
         file_path = received_data.split()[1].strip()
         data = load_file(file_path)
         loopbuf.append(log, data)
 
-        print("buffer size:",loopbuf.position_count)
+        print("buffer size:", loopbuf.position_count)
         if loopbuf.position_count >= BUFFER_SIZE:
             sample = loopbuf.sampler(
                 batch_size=BATCH_SIZE,
@@ -96,11 +92,10 @@ while True:
                 test=True,
             )
             batch = sample.next_batch()
-            train(batch)
+            communication_queue.put(batch)
 
-
-    # Close the server connection outside the loop
     if received_data == "shutdown":
         server.close()
         print("Connection closed.")
+        thread.join()
         break
