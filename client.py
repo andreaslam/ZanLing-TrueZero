cbf2ca24a539521bb9d9b27303161ffe9d38589e
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-09 18:20:33 +0000

    Update client.py

--- client.py
+++ client.py
@@ -20,12 +20,11 @@ class Server:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self) -> None:
-        connected = False
-        while not connected:
+        while True:
             try:
                 self.socket.connect((self.host, self.port))
                 print("Connected to server!")
-                connected = True
+                break
             except ConnectionRefusedError as e:
                 print(f"Connection failed: {e}. Retrying...")
                 continue
@@ -47,54 +46,56 @@ PORT = 8080
 BUFFER_SIZE = 1000
 BATCH_SIZE = 200
 
-# Initialization
-server = Server(HOST, PORT)
-server.connect()
+if __name__ == "__main__":
+    
+    # Initialization
+    server = Server(HOST, PORT)
+    server.connect()
 
-server.send("python-training")
+    server.send("python-training")
 
-loopbuf = LoopBuffer(
-    Game.find("chess"), target_positions=BUFFER_SIZE, test_fraction=0.2
-)
+    loopbuf = LoopBuffer(
+        Game.find("chess"), target_positions=BUFFER_SIZE, test_fraction=0.2
+    )
 
-communication_queue = Queue()
+    communication_queue = Queue()
 
-thread = threading.Thread(target=trainer_loop, args=(communication_queue,))
-thread.start()
+    thread = threading.Thread(target=trainer_loop, args=(communication_queue,))
+    thread.start()
 
-while True:
-    log = Logger()
-    log.start_batch()
-    received_data = server.receive()
-    print(f"Received: {received_data}")
+    while True:
+        log = Logger()
+        log.start_batch()
+        received_data = server.receive()
+        print(f"Received: {received_data}")
 
-    if (
-        "python-training" in received_data
-        or "rust-datagen" in received_data
-        or "requesting-net" in received_data
-    ):
-        server.send("newnet: chess_16x128_gen3634.pt")
+        if (
+            "python-training" in received_data
+            or "rust-datagen" in received_data
+            or "requesting-net" in received_data
+        ):
+            server.send("newnet: chess_16x128_gen3634.pt")
 
-    if "new-training-data" in received_data:
-        file_path = received_data.split()[1].strip()
-        data = load_file(file_path)
-        loopbuf.append(log, data)
+        if "new-training-data" in received_data:
+            file_path = received_data.split()[1].strip()
+            data = load_file(file_path)
+            loopbuf.append(log, data)
 
-        print("buffer size:", loopbuf.position_count)
-        if loopbuf.position_count >= BUFFER_SIZE:
-            sample = loopbuf.sampler(
-                batch_size=BATCH_SIZE,
-                unroll_steps=None,
-                include_final=False,
-                random_symmetries=False,
-                only_last_gen=False,
-                test=True,
-            )
-            batch = sample.next_batch()
-            communication_queue.put(batch)
+            print("buffer size:", loopbuf.position_count)
+            if loopbuf.position_count >= BUFFER_SIZE:
+                sample = loopbuf.sampler(
+                    batch_size=BATCH_SIZE,
+                    unroll_steps=None,
+                    include_final=False,
+                    random_symmetries=False,
+                    only_last_gen=False,
+                    test=True,
+                )
+                batch = sample.next_batch()
+                communication_queue.put(batch)
 
-    if received_data == "shutdown":
-        server.close()
-        print("Connection closed.")
-        thread.join()
-        break
+        if received_data == "shutdown":
+            server.close()
+            print("Connection closed.")
+            thread.join()
+            break
