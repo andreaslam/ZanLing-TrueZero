354899e3829e16c05032104cbcd86061db3ac95b
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-15 23:09:17 +0000

    Update server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,74 +1,108 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
-use std::thread;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use tz_rust::message_types::{ServerMessageRecv, ServerMessageSend};
 
 fn handle_client(
-    mut stream: TcpStream,
+    stream: TcpStream,
     clients: Arc<Mutex<Vec<TcpStream>>>,
     messages: Arc<Mutex<Vec<String>>>,
 ) {
-    let mut buffer = [0; 512];
+    let mut cloned_handle = stream.try_clone().unwrap();
+    let mut reader = BufReader::new(&stream);
     loop {
-        let bytes_read = match stream.read(&mut buffer) {
-            Ok(n) if n == 0 => {
-                break; // connection closed
-            }
-            Ok(n) => n,
-            Err(_) => {
-                break; // error while reading
+        let mut recv_msg = String::new();
+        if let Err(_) = reader.read_line(&mut recv_msg) {
+            return;
+        }
+
+        let message: ServerMessageSend = match serde_json::from_str(&recv_msg) {
+            Ok(msg) => msg,
+            Err(err) => {
+                eprintln!("[Error] Error deserialising message! {:?} {}", recv_msg, err);
+                recv_msg.clear();
+                continue;
             }
         };
 
-        let received = String::from_utf8(buffer[..bytes_read].to_vec()).unwrap();
-        println!("[Message] {}", received);
-        // keep track of messages sent and filter for python/rust instances
-
+        println!("[Message] {:?}", message);
         let mut all_messages = messages.lock().unwrap();
-        if received.clone().starts_with("python-training")
-            || received.clone().starts_with("rust-datagen")
-        {
-            let id: usize;
-            all_messages.push(received.clone());
-            // println!("{:?}", all_messages);
+        let purpose = message.purpose;
 
-            if received.clone().starts_with("python-training") {
-                id = all_messages
-                    .iter()
-                    .filter(|&n| *n == "python-training")
-                    .count()
-                    - 1;
-            } else if received.clone().starts_with("rust-datagen") {
-                id = all_messages
-                    .iter()
-                    .filter(|&n| *n == "rust-datagen")
-                    .count()
-                    - 1;
-            } else {
-                unreachable!();
+        if purpose == "initialise" {
+            let received = message.initialise_identity.unwrap();
+            if received.starts_with("python-training") || received.starts_with("rust-datagen") {
+                all_messages.push(received.clone());
+                let id = match received.as_str() {
+                    "python-training" => {
+                        all_messages
+                            .iter()
+                            .filter(|&n| *n == "python-training")
+                            .count()
+                            - 1
+                    }
+                    "rust-datagen" => {
+                        all_messages
+                            .iter()
+                            .filter(|&n| *n == "rust-datagen")
+                            .count()
+                            - 1
+                    }
+                    _ => {
+                        eprintln!("Impossible!");
+                        break;
+                    }
+                };
+
+                let message = ServerMessageRecv {
+                    verification: format!("{}: {}", received, id).into(),
+                    net_path: None,
+                };
+
+                let mut serialised = serde_json::to_string(&message).expect("serialization failed");
+                serialised += "\n";
+                if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
+                    eprintln!("Error sending identification! {}", msg);
+                    break;
+                }
+                println!("[Sent Identification] {:?}", message);
             }
-            let message = format!("{}: {} ", received, id);
-            // privately send identification
-            if let Err(_) = stream.write_all(message.as_bytes()) {}
-            println!("[Sent Identification] {}", message);
         } else {
-            // broadcast message to all clients except the sender
             let all_clients = clients.lock().unwrap();
             for mut client in all_clients.iter() {
-                if let Err(_) = client.write_all(received.as_bytes()) {}
-                println!("[Sent] {}", received);
+                if let Err(msg) = client.write_all(recv_msg.as_bytes()) {
+                    eprintln!("Error sending message to client! {}", msg);
+                    continue;
+                }
+                let mut disp_msg=  recv_msg.clone();
+                disp_msg.retain(|c| c != '\n'); // remove newline
+                println!("[Sent to {}]: {}", client.peer_addr().unwrap(), disp_msg);
             }
         }
-        // clear buffer
-        buffer = [0; 512];
+
+        if message.has_net {
+            let all_clients = clients.lock().unwrap();
+            for mut client in all_clients.iter() {
+                if let Err(msg) = client.write_all(recv_msg.as_bytes()) {
+                    eprintln!("Error sending message to client! {}", msg);
+                    continue;
+                }
+
+                let mut disp_msg =  recv_msg.clone();
+                disp_msg.retain(|c| c != '\n'); // remove newline
+                println!("[Sent to {}]: {}", client.peer_addr().unwrap(), disp_msg);
+            }
+        }
+
+        recv_msg.clear();
     }
 
-    println!(
-        "[Server] Client disconnected: {:?}",
-        stream.peer_addr().unwrap()
-    );
+    println!("[Server] Client disconnected!");
 }
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind address");
     let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
@@ -93,7 +127,7 @@ fn main() {
                 });
             }
             Err(e) => {
-                println!("Error: {}", e);
+                eprintln!("Error: {}", e);
             }
         }
     }
