c845c9f1ffb4504a1d5bce7c0ba76dea3a52772c
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-16 20:13:35 +0000

    Update network.py

--- network.py
+++ network.py
@@ -1,61 +1,62 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 
 class TrueNet(nn.Module):
     def __init__(self, num_resBlocks, num_hidden):
         super().__init__()
+        
         self.startBlock = nn.Sequential(
             nn.Conv2d(21, num_hidden, kernel_size=1, padding=0),
             nn.BatchNorm2d(num_hidden),
             nn.ReLU(),
         )
 
-        self.backBone = nn.Sequential(
-            *[ResBlock(num_hidden) for _ in range(num_resBlocks)]
-        )
+        self.backBone = nn.ModuleList([ResBlock(num_hidden) for _ in range(num_resBlocks)])
 
         self.policyHead = nn.Sequential(
             nn.Conv2d(num_hidden, num_hidden, kernel_size=1, padding=0),
             nn.BatchNorm2d(num_hidden),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(num_hidden * 64, 1880),  # input w*h
+            nn.Linear(num_hidden * 64, 1880),  
         )
 
         self.valueHead = nn.Sequential(
             nn.Conv2d(num_hidden, 1000, kernel_size=1, padding=0),
-            nn.BatchNorm2d(1000),
+            nn.BatchNorm2d(1000), 
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(
-                1000 * 64, 5
-            ), 
+            nn.Linear(1000 * 64, 5),
             nn.Tanh(),
         )
 
     def forward(self, x):
         x = self.startBlock(x)
-        x = self.backBone(x)
+        for block in self.backBone:
+            x_temp = block(x)
+            x = x_temp + x
+            x = F.relu(x)
+
         policy = self.policyHead(x)
         policy = policy.squeeze(0)
+
         value = self.valueHead(x)
         value = value.squeeze(0)
-        return value, policy
 
+        return value, policy
 
 class ResBlock(nn.Module):
     def __init__(self, num_hidden):
         super().__init__()
         self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(num_hidden)
-        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1) 
         self.bn2 = nn.BatchNorm2d(num_hidden)
 
     def forward(self, x):
         residual = x
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.bn2(self.conv2(x))
-        x += residual
-        x = F.relu(x)
+        x = x + residual
         return x
