8fd9a7b4448315b3719b18ee62af7eec22ea5c5d
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-18 22:24:06 +0000

    Update network.py

--- network.py
+++ network.py
@@ -1,29 +1,32 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class TrueNet(nn.Module):
     def __init__(self, num_resBlocks, num_hidden, head_nodes):
         super().__init__()
-        
+
         self.startBlock = nn.Sequential(
             nn.Conv2d(21, num_hidden, kernel_size=1, padding=0),
             nn.BatchNorm2d(num_hidden),
             nn.ReLU(),
         )
 
-        self.backBone = nn.ModuleList([ResBlock(num_hidden) for _ in range(num_resBlocks)])
+        self.backBone = nn.ModuleList(
+            [ResBlock(num_hidden) for _ in range(num_resBlocks)]
+        )
 
         self.policyHead = nn.Sequential(
             nn.Conv2d(num_hidden, head_nodes, kernel_size=1, padding=0),
             nn.BatchNorm2d(head_nodes),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(head_nodes * 64, 1880),  
+            nn.Linear(head_nodes * 64, 1880),
         )
 
         self.valueHead = nn.Sequential(
             nn.Conv2d(num_hidden, head_nodes, kernel_size=1, padding=0),
-            nn.BatchNorm2d(head_nodes), 
+            nn.BatchNorm2d(head_nodes),
             nn.ReLU(),
             nn.Flatten(),
             nn.Linear(head_nodes * 64, 5),
@@ -38,19 +41,18 @@ class TrueNet(nn.Module):
             x = F.relu(x)
 
         policy = self.policyHead(x)
-        policy = policy.squeeze(0)
 
         value = self.valueHead(x)
-        value = value.squeeze(0)
 
         return value, policy
+
 
 class ResBlock(nn.Module):
     def __init__(self, num_hidden):
         super().__init__()
         self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(num_hidden)
-        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1) 
+        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(num_hidden)
 
     def forward(self, x):
