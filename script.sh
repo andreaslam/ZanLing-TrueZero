0805c4dd210a2c2fd8053182ce99e89d4a69f0f7
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-20 11:48:40 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -46,6 +46,10 @@ export LIBTORCH_USE_PYTORCH=1
 export LIBTORCH_BYPASS_VERSION_CHECK=1 # just in case
 
 # Set LD_LIBRARY_PATH to PyTorch path
+# Find PyTorch installation directory
+PYTORCH_PATH=$(python -c "import torch; print(torch.__path__[0])")
+
+# Set LD_LIBRARY_PATH to PyTorch path
 export LD_LIBRARY_PATH="$PYTORCH_PATH/lib:$LD_LIBRARY_PATH"
 
 # build code
