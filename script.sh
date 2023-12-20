a351494812eaa7ff74ecd995cfb7ac9b73718428
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-20 06:15:18 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# utilities
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y curl wget git htop nano iputils-ping tree nethogs rsync unzip zip tmux
+apt-get upgrade -y git
+
+# speedtest
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash
+apt-get -y install speedtest
+
+# apt install -y valgrind
+
+# set max file handles to unlimited
+ulimit -u 2048
+
+# # .gitconfig
+# curl https://gist.githubusercontent.com/KarelPeeters/a3421a43e60524b3f12c8f626f7545d3/raw/ > ~/.gitconfig
+# git config --global credential.helper cache
+
+# rust & deps
+apt install -y pkg-config libssl-dev libclang-dev clang
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+. ~/.cargo/env
+
+# custom repos
+cd /workspace/
+git clone https://github.com/andreaslam/ZanLing-TrueZero
+
+# install project deps
+pip install -r ZanLing-TrueZero/requirements.txt
+apt install -y libglib2.0-0 libgl1 protobuf-compiler
+
+# build code
+cargo build --release
+cargo run --bin main --release
+cargo run --bin server --release
+python client.py
