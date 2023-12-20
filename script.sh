445230d8b698adda9d45371ab356d15ead9f3bdb
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-20 08:46:04 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,7 @@ apt-get -y install speedtest
 ulimit -u 2048
 
 # # .gitconfig
+# curl https://gist.githubusercontent.com/KarelPeeters/a3421a43e60524b3f12c8f626f7545d3/raw/ > ~/.gitconfig
 # git config --global credential.helper cache
 
 # rust & deps
@@ -25,6 +26,8 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # custom repos
 
+git clone https://github.com/andreaslam/ZanLing-TrueZero
+
 # install project deps
 pip install -r requirements.txt
 apt install -y libglib2.0-0 libgl1 protobuf-compiler
@@ -33,6 +36,8 @@ apt install -y libglib2.0-0 libgl1 protobuf-compiler
 
 export LIBTORCH_USE_PYTORCH=1
 export LIBTORCH_BYPASS_VERSION_CHECK=1 # just in case
+
+pip install darkdetect
 
 # build code
 cargo build --release 
