0eacfb7639b19639f5bbd1c1f5fc60df85cdff22
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-20 09:00:33 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -39,7 +39,7 @@ export LIBTORCH_BYPASS_VERSION_CHECK=1 # just in case
 
 pip install darkdetect
 pip install pyqtgraph
-conda install pyqtgraph
+pip3 install pyqtgraph
 
 # build code
 cargo build --release 
