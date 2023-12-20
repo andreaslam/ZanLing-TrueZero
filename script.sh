671a6da3a8bd6fa66964c156fcf27fbfa94ddaac
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-20 11:45:10 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,6 +41,8 @@ pip3 install pyqtgraph
 export LIBTORCH_USE_PYTORCH=1
 export LIBTORCH_BYPASS_VERSION_CHECK=1 # just in case
 
+# Set LD_LIBRARY_PATH to PyTorch path
+export LD_LIBRARY_PATH="$PYTORCH_PATH/lib:$LD_LIBRARY_PATH"
 
 # build code
 cargo build --release 
