52274d46794f1b1c2198a5fd6fbf9e2815e21d44
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-25 14:45:47 +0000

    Update network.py

--- network.py
+++ network.py
@@ -1,21 +1,20 @@
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.nn.init as init
+
 
 class TrueNet(nn.Module):
-    def __init__(self, num_resBlocks, num_hidden, head_channel_policy, head_channel_values):
+    def __init__(
+        self, num_resBlocks, num_hidden, head_channel_policy, head_channel_values
+    ):
         super().__init__()
-
         self.startBlock = nn.Sequential(
             nn.Conv2d(21, num_hidden, kernel_size=1, padding=0),
             nn.BatchNorm2d(num_hidden),
             nn.ReLU(),
         )
-
         self.backBone = nn.ModuleList(
             [ResBlock(num_hidden) for _ in range(num_resBlocks)]
         )
-
         self.policyHead = nn.Sequential(
             nn.Conv2d(num_hidden, head_channel_policy, kernel_size=1, padding=0),
             nn.BatchNorm2d(head_channel_policy),
@@ -23,10 +22,8 @@ class TrueNet(nn.Module):
             nn.Flatten(),
             nn.Linear(head_channel_policy * 64, 1880),
         )
-
         self.policyHead[-1].weight.data.fill_(0)
         self.policyHead[-1].bias.data.fill_(0)
-
         self.valueHead = nn.Sequential(
             nn.Conv2d(num_hidden, head_channel_values, kernel_size=1, padding=0),
             nn.BatchNorm2d(head_channel_values),
@@ -39,14 +36,9 @@ class TrueNet(nn.Module):
     def forward(self, x):
         x = self.startBlock(x)
         for block in self.backBone:
-            x_temp = block(x)
-            x = x_temp + x
-            x = F.relu(x)
-
+            x = F.relu(block(x) + x)
         policy = self.policyHead(x)
-
         value = self.valueHead(x)
-
         return value, policy
 
 
@@ -62,5 +54,36 @@ class ResBlock(nn.Module):
         residual = x
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.bn2(self.conv2(x))
-        x = x + residual
+        x += residual
         return x
+
+
+class TrueNetXS(nn.Module):
+    def __init__(self, num_hidden, head_channel_policy, head_channel_values):
+        super().__init__()
+        self.layerBlock = nn.Sequential(
+            nn.Conv2d(21, num_hidden, kernel_size=1, padding=0),
+            nn.BatchNorm2d(num_hidden),
+            nn.ReLU(),
+        )
+        self.policyHead = nn.Sequential(
+            nn.Conv2d(num_hidden, head_channel_policy, kernel_size=1, padding=0),
+            nn.BatchNorm2d(head_channel_policy),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(head_channel_policy * 64, 1880),
+        )
+        self.valueHead = nn.Sequential(
+            nn.Conv2d(num_hidden, head_channel_values, kernel_size=1, padding=0),
+            nn.BatchNorm2d(head_channel_values),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(head_channel_values * 64, 5),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        x = self.layerBlock(x)
+        policy = self.policyHead(x)
+        value = self.valueHead(x)
+        return value, policy
