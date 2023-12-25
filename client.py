ae3bc92e301fccd1672c33dcfab4d80371863552
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-25 10:53:27 +0000

    Update client.py

--- client.py
+++ client.py
@@ -274,7 +274,7 @@ def main():
                 data = load_file(file)
                 loopbuf.append(None, data)
                 print("[loaded files] buffer size:", loopbuf.position_count)
-            except FileNotFoundError:
+            except Exception:
                 continue
     try:
         log.load("log.npz")
