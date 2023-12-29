3e26720654cd3f7276a55d334082700d79cc48a2
andreaslam <81166794+andreaslam@users.noreply.github.com>
2023-12-29 16:53:06 +0000

    Update getgame.rs

--- src/bin/getgame.rs
+++ src/bin/getgame.rs
@@ -1,4 +1,4 @@
-use cozy_chess::{Board, GameStatus};
+use cozy_chess::{Board, GameStatus, Color};
 use crossbeam::thread;
 use flume::Sender;
 use std::{env, panic};
@@ -22,58 +22,93 @@ fn main() {
 
         std::process::exit(1);
     }));
+    let (tensor_exe_send_1, tensor_exe_recv_1) = flume::bounded::<Packet>(1);
+    let (tensor_exe_send_0, tensor_exe_recv_0) = flume::bounded::<Packet>(1);
+    
+    let mut scores = (0,0,0); // Win, Draw, Loss (White POV)
+    
+    let mut games_count = 0;
 
-    let board = Board::default();
-    let mut bs = BoardStack::new(board);
+    let mut target_games = 10;
 
     // set up executor and sender pairs
-
-    let (tensor_exe_send_0, tensor_exe_recv_0) = flume::bounded::<Packet>(1);
-    let (tensor_exe_send_1, tensor_exe_recv_1) = flume::bounded::<Packet>(1);
+    
     let (ctrl_sender, ctrl_recv) = flume::bounded::<Message>(1);
     let _ = thread::scope(|s| {
-        let tensor_exe_recv_clone_0 = tensor_exe_recv_0.clone();
-        let ctrl_recv_clone = ctrl_recv.clone();
-        s.builder()
-            .name("executor_net_0".to_string())
-            .spawn(move |_| {
-                executor_static(
-                    "./nets/tz_0.pt".to_string(),
-                    // "chess_16x128_gen3634.pt".to_string(),
-                    tensor_exe_recv_clone_0,
-                    ctrl_recv_clone,
-                    1,
-                )
-            })
-            .unwrap();
-        let tensor_exe_recv_clone_1 = tensor_exe_recv_1.clone();
-        let ctrl_recv_clone = ctrl_recv.clone();
-        s.builder()
-            .name("executor_net_1".to_string())
-            .spawn(move |_| {
-                executor_static(
-                    "./nets/tz_0.pt".to_string(),
-                    // "chess_16x128_gen3634.pt".to_string(),
-                    tensor_exe_recv_clone_1,
-                    ctrl_recv_clone,
-                    1,
-                )
-            })
-            .unwrap();
-        let mut counter = 0;
-        let mut tensor_exe_send: Sender<Packet>;
-        while bs.status() == GameStatus::Ongoing {
-            if counter % 2 == 0 {
-                tensor_exe_send = tensor_exe_send_0.clone();
-            } else {
-                tensor_exe_send = tensor_exe_send_1.clone();
-            }
-            let (mv, _, _, _, _) = get_move(bs.clone(), tensor_exe_send.clone());
-            bs.play(mv);
-            println!("{:#}", mv);
+        while games_count < target_games {
 
-            counter += 1;
+            let board = Board::default();
+            let mut bs = BoardStack::new(board);
+            let tensor_exe_recv_clone_0 = tensor_exe_recv_0.clone();
+            let ctrl_recv_clone = ctrl_recv.clone();
+            s.builder()
+                .name("executor_net_0".to_string())
+                .spawn(move |_| {
+                    executor_static(
+                        "./nets/tz_1161.pt".to_string(),
+                        // "chess_16x128_gen3634.pt".to_string(),
+                        tensor_exe_recv_clone_0,
+                        ctrl_recv_clone,
+                        1,
+                    )
+                })
+                .unwrap();
+            let tensor_exe_recv_clone_1 = tensor_exe_recv_1.clone();
+            let ctrl_recv_clone = ctrl_recv.clone();
+            s.builder()
+                .name("executor_net_1".to_string())
+                .spawn(move |_| {
+                    executor_static(
+                        "chess_16x128_gen3634.pt".to_string(),
+                        // "chess_16x128_gen3634.pt".to_string(),
+                        tensor_exe_recv_clone_1,
+                        ctrl_recv_clone,
+                        1,
+                    )
+                })
+                .unwrap();
+            let mut counter = 0;
+            let mut tensor_exe_send: Sender<Packet>;
+            while bs.status() == GameStatus::Ongoing {
+                if counter % 2 == 0 {
+                    tensor_exe_send = tensor_exe_send_0.clone();
+                } else {
+                    tensor_exe_send = tensor_exe_send_1.clone();
+                }
+                let (mv, _, _, _, _) = get_move(bs.clone(), tensor_exe_send.clone());
+                bs.play(mv);
+                println!("{:#}", mv);
+
+                counter += 1;
+            }
+            let result = match bs.status() {
+                GameStatus::Ongoing => panic!("game not over yet!"),
+                GameStatus::Won => Some(!bs.board().side_to_move()),
+                GameStatus::Drawn => None,
+            };
+
+            match result {
+                Some(winner) => {
+                    match winner {
+                        Color::White => {
+                            println!("1-0");
+                            scores.0 += 1;
+                        },
+                        Color::Black => {
+                            println!("0-1");
+                            scores.2 += 1;
+                        },
+                    }
+                }
+                None => {
+                    println!("1/2-1/2");
+                    scores.1 += 1;
+                },
+            }
+
+            games_count +=1;
         }
+        println!("{:?}", scores);
         ctrl_sender.send(StopServer()).unwrap();
-    });
+    }).unwrap();
 }
