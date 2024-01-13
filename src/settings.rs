334f91acdbd6ca14b04eb1db8794db32a0b79c6b
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-01-13 16:32:36 +0000

    Update settings.rs

--- src/settings.rs
+++ src/settings.rs
@@ -1,10 +1,14 @@
-use crate::mcts_trainer::Wdl;
+use crate::mcts_trainer::{TypeRequest, Wdl};
+
+#[derive(Clone, Debug, PartialEq, Copy)]
 
 pub struct SearchSettings {
-    fpu: f32,
-    wdl: Wdl,
-    c_puct: f32,
-    max_nodes: u32,
-    alpha: f32,
-    eps: f32,
+    pub fpu: f32,
+    pub wdl: Option<Wdl>, // if WDL is None then automatically use value
+    pub moves_left: Option<usize>,
+    pub c_puct: f32,
+    pub max_nodes: u128,
+    pub alpha: f32,
+    pub eps: f32,
+    pub search_type: TypeRequest,
 }
