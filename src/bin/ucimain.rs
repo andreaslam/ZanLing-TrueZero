bf97e6997dcee19754632cbee05bc425d48acf02
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-01-17 23:22:11 +0000

    Update ucimain.rs

--- src/bin/ucimain.rs
+++ src/bin/ucimain.rs
@@ -1,6 +1,8 @@
+use std::env;
+
 use tz_rust::uci::run_uci;
 
-
 fn main() {
-    run_uci();
+    env::set_var("RUST_BACKTRACE", "2");
+    run_uci(r"C:\Users\andre\RemoteFolder\ZanLing-TrueZero\nets\tz_5151.pt");
 }
