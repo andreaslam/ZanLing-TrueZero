dfc69f61ec546943bde0d7a840914ab15b6bf817
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-02-04 00:33:03 +0000

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused)]
 use crossbeam::thread;
 use flume::{Receiver, Sender};
 use rand::prelude::*;
@@ -9,16 +11,17 @@ use std::{
     panic,
     time::{Duration, Instant},
 };
+use tokio::task::spawn;
 use tz_rust::{
     executor::{executor_main, Packet},
     fileformat::BinaryOutput,
     mcts_trainer::TypeRequest::TrainerSearch,
     message_types::{DataFileType, Entity, MessageServer, MessageType, Statistics},
-    selfplay::{synthetic_expansion, CollectorMessage, DataGen},
+    selfplay::{CollectorMessage, DataGen},
     settings::SearchSettings,
 };
-
-fn main() {
+#[tokio::main]
+async fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     panic::set_hook(Box::new(|panic_info| {
         // print panic information
@@ -38,6 +41,7 @@ fn main() {
     let message = MessageServer {
         purpose: MessageType::Initialise(Entity::RustDataGen),
     };
+    let mut handles_vec = vec![];
     let mut serialised = serde_json::to_string(&message).expect("serialisation failed");
     serialised += "\n";
     stream
@@ -45,8 +49,9 @@ fn main() {
         .expect("Failed to send data");
     println!("Connected to server!");
     let (game_sender, game_receiver) = flume::bounded::<CollectorMessage>(1);
-    let num_threads = 1024;
     let num_executors = 2;
+    let batch_size = 1024; // executor batch size
+    let num_generators = num_executors * batch_size * 2;
     thread::scope(|s| {
         let mut selfplay_masters: Vec<DataGen> = Vec::new();
         // commander
@@ -64,7 +69,8 @@ fn main() {
 
         let (id_send, id_recv) = flume::bounded::<usize>(1);
 
-        s.builder()
+        let _ = s
+            .builder()
             .name("commander".to_string())
             .spawn(|_| {
                 commander_main(
@@ -76,30 +82,32 @@ fn main() {
             .unwrap();
         // selfplay threads
         let (tensor_exe_send, tensor_exe_recv) = flume::bounded::<Packet>(1); // mcts to executor
-        for n in 0..num_threads {
+        for n in 0..num_generators {
             // // executor
             // sender-receiver pair to communicate for each thread instance to the executor
             let sender_clone = game_sender.clone();
             let mut selfplay_master = DataGen { iterations: 1 };
             let tensor_exe_send_clone = tensor_exe_send.clone();
             let nps_sender = game_sender.clone();
-            s.builder()
-                .name(format!("generator_{}", n.to_string()))
-                .spawn(move |_| {
-                    generator_main(
-                        &sender_clone,
-                        &mut selfplay_master,
-                        tensor_exe_send_clone,
-                        nps_sender,
-                    )
-                })
-                .unwrap();
+
+            let handle = spawn(async move {
+                generator_main(
+                    sender_clone,
+                    selfplay_master,
+                    tensor_exe_send_clone,
+                    nps_sender,
+                    n,
+                )
+                .await
+            });
+            handles_vec.push(handle);
 
             selfplay_masters.push(selfplay_master.clone());
         }
         // collector
 
-        s.builder()
+        let _ = s
+            .builder()
             .name("collector".to_string())
             .spawn(|_| {
                 collector_main(
@@ -115,13 +123,13 @@ fn main() {
             // send/recv pair between executor and commander
             let eval_per_sec_sender = game_sender.clone();
             let tensor_exe_recv_clone = tensor_exe_recv.clone();
-            s.builder()
+            let _ = s.builder()
                 .name(format!("executor_{}", n.to_string()))
                 .spawn(move |_| {
                     executor_main(
                         communicate_exe_recv,
                         tensor_exe_recv_clone,
-                        num_threads / num_executors,
+                        batch_size,
                         eval_per_sec_sender,
                     )
                 })
@@ -130,13 +138,19 @@ fn main() {
         }
     })
     .unwrap();
+    for handle in handles_vec {
+        if let Err(e) = handle.await {
+            eprintln!("Error joining thread: {:?}", e);
+        }
+    }
 }
 
-fn generator_main(
-    sender_collector: &Sender<CollectorMessage>,
-    datagen: &mut DataGen,
+async fn generator_main(
+    sender_collector: Sender<CollectorMessage>,
+    datagen: DataGen,
     tensor_exe_send: Sender<Packet>,
     nps_sender: Sender<CollectorMessage>,
+    id: usize,
 ) {
     let settings: SearchSettings = SearchSettings {
         fpu: 0.0,
@@ -150,43 +164,47 @@ fn generator_main(
         pst: 1.2,
     };
     loop {
-        let sim = datagen.play_game(
-            tensor_exe_send.clone(),
-            nps_sender.clone(),
-            settings.clone(),
-        );
+        let sim = datagen
+            .play_game(
+                tensor_exe_send.clone(),
+                nps_sender.clone(),
+                settings.clone(),
+                id,
+            )
+            .await;
 
-        match settings.search_type {
-            TrainerSearch(expansiontype) => match expansiontype {
-                Some(_) => {
-                    let num_positions_sample =
-                        rand::thread_rng().gen_range(1..=sim.positions.len());
-                    let mut rng = rand::thread_rng();
-                    let random_idx: Vec<usize> = (0..num_positions_sample)
-                        .map(|_| rng.gen_range(0..=sim.positions.len() - 1))
-                        .collect();
+        // match settings.search_type {
+        //     TrainerSearch(expansiontype) => match expansiontype {
+        //         Some(_) => {
+        //             let num_positions_sample =
+        //                 rand::thread_rng().gen_range(1..=sim.positions.len());
+        //             let mut rng = rand::thread_rng();
+        //             let random_idx: Vec<usize> = (0..num_positions_sample)
+        //                 .map(|_| rng.gen_range(0..=sim.positions.len() - 1))
+        //                 .collect();
 
-                    for position in random_idx {
-                        let sim = synthetic_expansion(
-                            sim.clone(),
-                            position,
-                            tensor_exe_send.clone(),
-                            settings.clone(),
-                        );
-                        sender_collector
-                            .send(CollectorMessage::FinishedGame(sim))
-                            .unwrap();
-                    }
-                }
-                None => {}
-            },
-            tz_rust::mcts_trainer::TypeRequest::SyntheticSearch => {}
-            tz_rust::mcts_trainer::TypeRequest::NonTrainerSearch => {}
-            tz_rust::mcts_trainer::TypeRequest::UCISearch => {}
-        }
+        //             for position in random_idx {
+        //                 let sim = synthetic_expansion(
+        //                     sim.clone(),
+        //                     position,
+        //                     tensor_exe_send.clone(),
+        //                     settings.clone(),
+        //                 );
+        //                 sender_collector
+        //                     .send(CollectorMessage::FinishedGame(sim))
+        //                     .unwrap();
+        //             }
+        //         }
+        //         None => {}
+        //     },
+        //     tz_rust::mcts_trainer::TypeRequest::SyntheticSearch => {}
+        //     tz_rust::mcts_trainer::TypeRequest::NonTrainerSearch => {}
+        //     tz_rust::mcts_trainer::TypeRequest::UCISearch => {}
+        // }
 
         sender_collector
-            .send(CollectorMessage::FinishedGame(sim))
+            .send_async(CollectorMessage::FinishedGame(sim))
+            .await
             .unwrap();
     }
 }
@@ -241,9 +259,9 @@ fn collector_main(
         let msg = receiver.recv().unwrap();
         match msg {
             CollectorMessage::FinishedGame(sim) => {
-                let _ = bin_output.append(&sim).unwrap();
+                bin_output.append(&sim).unwrap();
                 if bin_output.game_count() >= 3 {
-                    let _ = bin_output.finish().unwrap();
+                    bin_output.finish().unwrap();
 
                     for file in files {
                         let file_path = format!("{}{}", path, file);
@@ -329,6 +347,7 @@ fn collector_main(
         }
     }
 }
+
 fn commander_main(
     vec_exe_sender: Vec<Sender<String>>,
     server_handle: &mut TcpStream,
@@ -370,8 +389,11 @@ fn commander_main(
                 MessageType::IdentityConfirmation(_) => {}
                 MessageType::JobSendData(_) => {}
                 MessageType::NewNetworkData(data) => {
-                    println!("SUP");
-                    net_path = format!("./nets/tz_temp_net_{}_{}.pt", generator_id, net_path_counter);
+                    println!("new net path");
+                    net_path = format!(
+                        "./nets/tz_temp_net_{}_{}.pt",
+                        generator_id, net_path_counter
+                    );
                     let mut file = File::create(net_path.clone()).expect("Unable to create file");
                     file.write_all(&data).expect("Unable to write data");
                     net_path_counter += 1;
@@ -398,8 +420,7 @@ fn commander_main(
                     }
                 },
                 MessageType::JobSendData(_) => {}
-                MessageType::NewNetworkData(_) => {
-                }
+                MessageType::NewNetworkData(_) => {}
             }
         }
 
@@ -407,11 +428,11 @@ fn commander_main(
             if !net_path.is_empty() {
                 println!("updating net to: {}", net_path.clone());
                 match fs::remove_file(curr_net.clone()) {
-                            Ok(_) => {
-                                println!("Deleted net {}", curr_net);
-                            }
-                            Err(e) => eprintln!("Error deleting the file: {}", e),
-                        }
+                    Ok(_) => {
+                        println!("Deleted net {}", curr_net);
+                    }
+                    Err(e) => eprintln!("Error deleting the file: {}", e),
+                }
                 for exe_sender in &vec_exe_sender {
                     exe_sender.send(net_path.clone()).unwrap();
                     // println!("SENT!");
