f1055616e6ec02cabd49cbf30a05c21d9bc290e5
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-02-10 21:01:53 +0000

    Update executor.rs

--- src/executor.rs
+++ src/executor.rs
@@ -13,6 +13,17 @@ pub struct Packet {
     pub id: String,
 }
 
+struct CacheEntry {
+    position: Tensor,
+    output: (Tensor, Tensor),
+}
+
+impl CacheEntry {
+    fn new(position: Tensor, output: (Tensor, Tensor)) -> Self {
+        Self { position, output }
+    }
+}
+
 pub struct ReturnPacket {
     pub packet: (Tensor, Tensor),
     pub id: String,
@@ -57,17 +68,20 @@ pub fn executor_main(
     let mut output_senders = VecDeque::new(); // collect senders
     let mut id_vec = VecDeque::new(); // collect senders
                                       // // println!("num_threads (generator): {}", num_threads);
-
+    let mut cache: Vec<CacheEntry> = Vec::new();
+    let mut cache_hits = 0;
+    let mut requests = 0;
     loop {
         let sw = Instant::now();
         // println!("thread {} loop {}:", thread_name, debug_counter);
-        // println!(
+
+        // // println!(
         //     "    thread {} number of requests made from mcts: {}, {}",
         //     thread_name,
         //     output_senders.len(),
         //     batch_size
         // );
-        // println!(
+        // // println!(
         //     "    thread {} waiting graph_disconnected: {}",
         //     thread_name, graph_disconnected
         // );
@@ -93,68 +107,124 @@ pub fn executor_main(
             Message::NewNetwork(Ok(graph)) => {
                 // println!("    NEW NET!");
                 handle_new_graph(&mut network, Some(graph), &thread_name);
+                cache = Vec::new();
             }
             Message::JobTensor(job) => {
+                requests += 1;
                 // // println!("    received job");
                 let job = job.expect("JobTensor should be available");
                 let network = network.as_mut().expect("Network should be available");
-
+                let is_contained = cache
+                    .iter()
+                    .any(|cache_entry| cache_entry.position == job.job);
+                let cache_idx = cache
+                    .iter()
+                    .position(|cache_entry| cache_entry.position == job.job);
                 input_vec.push_back(job.job);
                 output_senders.push_back(job.resender);
                 id_vec.push_back(job.id);
-                // println!(
-                //     "    received job, {}, batch_size {}",
-                //     input_vec.len(),
-                //     batch_size
-                // );
-                // evaluate batches
-                while input_vec.len() >= batch_size {
-                    let batch_size = min(batch_size, input_vec.len());
-                    let i_v = input_vec.make_contiguous();
-                    let input_tensors = Tensor::cat(&i_v[..batch_size], 0);
-                    // // println!("        thread {}: preparing tensors", thread_name);
-                    // // println!(
+                // // println!(
+                    //     "    received job, {}, batch_size {}",
+                    //     input_vec.len(),
+                    //     batch_size
+                    // );
+                    // evaluate batches
+                    
+                    if is_contained {
+                    let sender = output_senders
+                        .pop_back()
+                        .expect("There should be a sender for each job");
+                    let id = id_vec
+                        .pop_back()
+                        .expect("There should be an ID for each job");
+                    let value = &cache[cache_idx.unwrap()].output.0;
+                    let policy = &cache[cache_idx.unwrap()].output.1;
+                    // // println!("            thread {}, SENT! {:?}", i, &result);
+                    // // println!("result {:?}", (value, policy));
+                    let return_pack = ReturnPacket {
+                        packet: (value.clone(value), policy.clone(policy)),
+                        id,
+                    };
+                    sender
+                        .send(ReturnMessage::ReturnMessage(Ok(return_pack)))
+                        .expect("Should be able to send the result");
+                    input_vec.pop_back();
+                    cache_hits += 1;
+                    println!("cache size {} cache hits {}, total requests {}, cache satisfaction {:.3}%", cache.len(), cache_hits, requests, ((cache_hits as f32 )/(requests as f32) * 100.0));
+                } else {
+                    while input_vec.len() >= batch_size {
+                        let waiting_time = sw.elapsed().as_nanos() as f32 / 1e9; // waiting time in seconds
+                                                                                 // // // println!(
+                                                                                 //     "        thread name {}, waiting time :{}s",
+                                                                                 //     thread_name, waiting_time
+                                                                                 // );
+                        let batch_size = min(batch_size, input_vec.len());
+                        let i_v = input_vec.make_contiguous();
+                        let input_tensors = Tensor::cat(&i_v[..batch_size], 0);
+                        // // println!("        thread {}: preparing tensors", thread_name);
+                        // // println!(
                         // "            thread {}: eval input tensors: {:?}",
                         // thread_name, input_tensors
-                    // );
-                    // println!("        thread {}: NN evaluation:", thread_name);
-                    let sw_inference = Instant::now();
-                    let (board_eval, policy) =
-                        eval_state(input_tensors, network).expect("Evaluation failed");
-                    let elapsed = sw_inference.elapsed().as_nanos() as f32 / 1e9;
-                    // let evals_per_sec = batch_size as f32 / elapsed;
-                    evals_per_sec_sender
-                        .send(CollectorMessage::ExecutorStatistics(batch_size as f32)).unwrap();
-                    // // println!(
-                    //     "            thread {}: NN evaluation done! {}s",
-                    //     thread_name, elapsed
-                    // );
-                    // // println!("        thread {}: processing outputs:", thread_name);
-                    // // println!(
-                    //     "            thread {}: output tensors: {:?}, {:?}",
-                    //     thread_name, board_eval, policy
-                    // );
-                    // distribute results to the output senders
-                    // println!(
-                    //     "        thread {}: sending tensors back to mcts:",
-                    //     thread_name
-                    // );
-                    for i in 0..batch_size {
-                        let sender = output_senders
-                            .pop_front()
-                            .expect("There should be a sender for each job");
-                        let id = id_vec
-                            .pop_front()
-                            .expect("There should be an ID for each job");
-                        let result = (board_eval.get(i as i64), policy.get(i as i64));
-                        // // println!("            thread {}, SENT! {:?}", i, &result);
-                        let return_pack = ReturnPacket { packet: result, id };
-                        sender
-                            .send(ReturnMessage::ReturnMessage(Ok(return_pack)))
-                            .expect("Should be able to send the result");
+                        // );
+                        // println!("        thread {}: NN evaluation:", thread_name);
+                        let sw_inference = Instant::now();
+                        let (board_eval, policy) =
+                            eval_state(input_tensors.clone(&input_tensors), network)
+                                .expect("Evaluation failed");
+
+                        let elapsed = sw_inference.elapsed().as_nanos() as f32 / 1e9;
+                        // println!("        inference time: {}s", elapsed);
+                        // let evals_per_sec = batch_size as f32 / elapsed;
+                        evals_per_sec_sender
+                            .send(CollectorMessage::ExecutorStatistics(batch_size as f32))
+                            .unwrap();
+                        // println!("sent to server");
+                        // // println!(
+                            //     "            thread {}: NN evaluation done! {}s",
+                            //     thread_name, elapsed
+                            // );
+                            // // println!("        thread {}: processing outputs:", thread_name);
+                            // // println!(
+                        //     "            thread {}: output tensors: {:?}, {:?}",
+                        //     thread_name, board_eval, policy
+                        // );
+                        // distribute results to the output senders
+                        // // println!(
+                        //     "        thread {}: sending tensors back to mcts:",
+                        //     thread_name
+                        // );
+                        let batch_size = min(batch_size, input_vec.len());
+                        for i in 0..batch_size {
+                            let sender = output_senders
+                                .pop_front()
+                                .expect("There should be a sender for each job");
+                            let id = id_vec
+                                .pop_front()
+                                .expect("There should be an ID for each job");
+                            let result = (board_eval.get(i as i64), policy.get(i as i64));
+                            // // println!("            thread {}, SENT! {:?}", i, &result);
+                            let return_pack = ReturnPacket { packet: result, id };
+                            sender
+                                .send(ReturnMessage::ReturnMessage(Ok(return_pack)))
+                                .expect("Should be able to send the result");
+                            let (board_eval, policy) =
+                                (board_eval.get(i as i64), policy.get(i as i64));
+                            if (cache.iter().any(|cache_entry| {
+                                cache_entry.output
+                                    == (board_eval.clone(&board_eval), policy.clone(&policy))
+                            })) == false
+                            {
+                                // println!("added to cache {:?}", input_tensors.reshape([-1,1344]).get(i as i64));
+                                cache.push(CacheEntry::new(
+                                    input_tensors.reshape([-1,1344]).get(i as i64),
+                                    (board_eval.clone(&board_eval), policy.clone(&policy)),
+                                ));
+                                // println!("progress {}, cache len {}", i, cache.len());
+                            }
+                        }
+                        // println!("    {} sent all evals back!", thread_name);
+                        drop(input_vec.drain(0..batch_size));
                     }
-                    // println!("    {} sent all evals back!", thread_name);
-                    drop(input_vec.drain(0..batch_size));
                 }
             }
             Message::NewNetwork(Err(RecvError::Disconnected)) => {
@@ -209,12 +279,16 @@ pub fn executor_static(
             Message::JobTensor(job) => {
                 let job = job.expect("JobTensor should be available");
                 let network = network.as_mut().expect("Network should be available");
-
                 input_vec.push_back(job.job);
                 output_senders.push_back(job.resender);
                 id_vec.push_back(job.id);
 
                 while input_vec.len() >= max_batch_size {
+                    let waiting_time = sw.elapsed().as_nanos() as f32 / 1e9; // waiting time in seconds
+                    // // println!(
+                    //     "thread name {}, waiting time :{}s",
+                    //     thread_name, waiting_time
+                    // );
                     let batch_size = min(max_batch_size, input_vec.len());
                     let i_v = input_vec.make_contiguous();
                     let input_tensors = Tensor::cat(&i_v[..batch_size], 0);
@@ -223,7 +297,7 @@ pub fn executor_static(
                     let (board_eval, policy) =
                         eval_state(input_tensors, network).expect("Evaluation failed");
                     let elapsed = sw_inference.elapsed().as_nanos() as f32 / 1e9;
-
+                    // println!("elapsed: {}", elapsed);
                     for i in 0..batch_size {
                         let sender = output_senders
                             .pop_front()
