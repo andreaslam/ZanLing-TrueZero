2a2bf70a2c2c3800f37f1214f03930c7b70b2471
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-03-26 17:07:35 +0000

    Update server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -26,189 +26,182 @@ fn handle_client(
             recv_msg.clear();
             continue;
         }
-        // println!("{:?}", recv_msg);
-        let message: MessageServer = match serde_json::from_str(&recv_msg) {
-            Ok(msg) => msg,
-            Err(err) => {
-                eprintln!(
-                    "[Error] Error deserialising message! {:?} {}",
-                    recv_msg, err
-                );
-                recv_msg.clear();
-                continue;
-            }
-        };
 
-        let mut all_messages = messages.lock().unwrap();
-        let mut net_path = net_path.lock().unwrap();
-        let mut net_data = net_data.lock().unwrap();
-
-        // TODO: find more msg types to NOT save to all_messages
-
-        match message.purpose {
-            MessageType::StatisticsSend(_) => {},
-            _ => all_messages.push(message.clone()),
-        }
-        let purpose = message.purpose;
-        match purpose {
-            MessageType::Initialise(entity) => {
-                let message_send: MessageServer;
-                match entity {
-                    Entity::RustDataGen => {
-                        let id = all_messages
-                            .iter()
-                            .filter(|&n| {
-                                *n == MessageServer {
-                                    purpose: MessageType::Initialise(Entity::RustDataGen),
-                                }
-                            })
-                            .count()
-                            - 1;
-                        // println!("id {}", id);
-                        message_send = MessageServer {
-                            purpose: MessageType::IdentityConfirmation((entity, id)),
-                        };
-                    }
-                    Entity::PythonTraining => {
-                        let id = all_messages
-                            .iter()
-                            .filter(|&n| {
-                                *n == MessageServer {
-                                    purpose: MessageType::Initialise(Entity::PythonTraining),
-                                }
-                            })
-                            .count()
-                            - 1;
-                        message_send = MessageServer {
-                            purpose: MessageType::IdentityConfirmation((entity, id)),
-                        };
-                        if !has_net {
-                            let extra_request = MessageServer {
-                                purpose: MessageType::RequestingNet,
-                            };
-                            let mut serialised = serde_json::to_string(&extra_request)
-                                .expect("serialization failed");
-                            serialised += "\n";
-                            if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
-                                eprintln!("Error sending identification! {}", msg);
-                                break;
-                            } else {
-                                println!("[Server] Requesting net");
-                            }
-                        }
-                    }
-                    Entity::GUIMonitor => {
-                        let id = all_messages
-                            .iter()
-                            .filter(|&n| {
-                                *n == MessageServer {
-                                    purpose: MessageType::Initialise(Entity::GUIMonitor),
-                                }
-                            })
-                            .count()
-                            - 1;
-                        message_send = MessageServer {
-                            purpose: MessageType::IdentityConfirmation((entity, id)),
-                        };
-                    }
-                }
-                let mut serialised =
-                    serde_json::to_string(&message_send).expect("serialization failed");
-                serialised += "\n";
-                if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
-                    eprintln!("Error sending identification! {}", msg);
-                    break;
-                } else {
-                    println!(
-                        "[Sent Identification to {}] {:?}",
-                        cloned_handle.peer_addr().unwrap(),
-                        message_send
+        if !recv_msg.is_empty() {
+            let message: MessageServer = match serde_json::from_str(&recv_msg) {
+                Ok(msg) => msg,
+                Err(err) => {
+                    eprintln!(
+                        "[Error] Error deserialising message! {:?} {}",
+                        recv_msg, err
                     );
+                    recv_msg.clear();
+                    continue;
                 }
-                recv_msg.clear();
-                continue;
-            }
-            MessageType::JobSendPath(_) => {}
-            MessageType::StatisticsSend(statistics) => {
-                let mut stats = stats_counters.lock().unwrap_or_else(|e| e.into_inner());
-                let mut start_time = start_time.lock().unwrap_or_else(|e| e.into_inner());
-                let elapsed = start_time.elapsed().as_secs_f32();
-                match statistics {
-                    tz_rust::message_types::Statistics::NodesPerSecond(nps) => {
-                        stats.0 += nps;
-                    }
-                    tz_rust::message_types::Statistics::EvalsPerSecond(evals_per_sec) => {
-                        stats.1 += evals_per_sec;
-                    }
-                }
-                if elapsed >= 1.0 {
-                    println!("[Statistics-nps] {}", stats.0);
-                    println!("[Statistics-evals] {}", stats.1);
-                    *stats = (0.0, 0.0);
-                    *start_time = Instant::now();
-                }
-                recv_msg.clear();
-                continue;
-            }
-            MessageType::RequestingNet => {
-                if !has_net {
-                    match *net_data {
-                        Some(ref path) => {
-                            let extra_request = MessageServer {
-                                // purpose: MessageType::NewNetworkPath(path.clone()),
-                                purpose: MessageType::NewNetworkData(path.clone()),
+            };
+
+            let mut all_messages = messages.lock().unwrap();
+            let mut net_path = net_path.lock().unwrap();
+            let mut net_data = net_data.lock().unwrap();
+
+            // TODO: find more msg types to NOT save to all_messages
+            let saved_msg: MessageServer = message.clone();
+            let purpose = message.purpose;
+            match purpose {
+                MessageType::Initialise(entity) => {
+                    let message_send: MessageServer;
+                    match entity {
+                        Entity::RustDataGen => {
+                            all_messages.push(saved_msg.clone());
+                            let id = all_messages
+                                .iter()
+                                .filter(|&n| {
+                                    *n == MessageServer {
+                                        purpose: MessageType::Initialise(Entity::RustDataGen),
+                                    }
+                                })
+                                .count()
+                                - 1;
+                            // println!("id {}", id);
+                            message_send = MessageServer {
+                                purpose: MessageType::IdentityConfirmation((entity, id)),
                             };
-                            let mut serialised = serde_json::to_string(&extra_request)
-                                .expect("serialisation failed");
-                            serialised += "\n";
-                            if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
-                                eprintln!("Error sending identification! {}", msg);
-                                break;
-                            } else {
-                                // println!("[Server] Sending net path {}", path.clone());
+                        }
+                        Entity::PythonTraining => {
+                            all_messages.push(saved_msg.clone());
+                            let id = all_messages
+                                .iter()
+                                .filter(|&n| {
+                                    *n == MessageServer {
+                                        purpose: MessageType::Initialise(Entity::PythonTraining),
+                                    }
+                                })
+                                .count()
+                                - 1;
+                            message_send = MessageServer {
+                                purpose: MessageType::IdentityConfirmation((entity, id)),
+                            };
+                            if !has_net {
+                                let extra_request = MessageServer {
+                                    purpose: MessageType::RequestingNet,
+                                };
+                                let mut serialised = serde_json::to_string(&extra_request)
+                                    .expect("serialization failed");
+                                serialised += "\n";
+                                if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
+                                    eprintln!("Error sending identification! {}", msg);
+                                    break;
+                                } else {
+                                    println!("[Server] Requesting net");
+                                }
                             }
                         }
-                        None => {}
+                        Entity::GUIMonitor => {
+                            all_messages.push(saved_msg.clone());
+                            let id = all_messages
+                                .iter()
+                                .filter(|&n| {
+                                    *n == MessageServer {
+                                        purpose: MessageType::Initialise(Entity::GUIMonitor),
+                                    }
+                                })
+                                .count()
+                                - 1;
+                            message_send = MessageServer {
+                                purpose: MessageType::IdentityConfirmation((entity, id)),
+                            };
+                        }
+                    }
+                    let mut serialised =
+                        serde_json::to_string(&message_send).expect("serialization failed");
+                    serialised += "\n";
+                    if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
+                        eprintln!("Error sending identification! {}", msg);
+                        break;
+                    } else {
+                        println!(
+                            "[Sent Identification to {}] {:?}",
+                            cloned_handle.peer_addr().unwrap(),
+                            message_send
+                        );
                     }
                     recv_msg.clear();
                     continue;
-                } else {
-                    has_net = false;
+                }
+                MessageType::JobSendPath(_) => {}
+                MessageType::StatisticsSend(statistics) => {
+                    let mut stats = stats_counters.lock().unwrap_or_else(|e| e.into_inner());
+                    let mut start_time = start_time.lock().unwrap_or_else(|e| e.into_inner());
+                    let elapsed = start_time.elapsed().as_secs_f32();
+                    match statistics {
+                        tz_rust::message_types::Statistics::NodesPerSecond(nps) => {
+                            stats.0 += nps;
+                        }
+                        tz_rust::message_types::Statistics::EvalsPerSecond(evals_per_sec) => {
+                            stats.1 += evals_per_sec;
+                        }
+                    }
+                    if elapsed >= 1.0 {
+                        println!("[Statistics-nps] {}", stats.0);
+                        println!("[Statistics-evals] {}", stats.1);
+                        *stats = (0.0, 0.0);
+                        *start_time = Instant::now();
+                    }
+                    recv_msg.clear();
+                    continue;
+                }
+                MessageType::RequestingNet => {
+                    if !has_net {
+                        match *net_data {
+                            Some(ref path) => {
+                                let extra_request = MessageServer {
+                                    // purpose: MessageType::NewNetworkPath(path.clone()),
+                                    purpose: MessageType::NewNetworkData(path.clone()),
+                                };
+                                let mut serialised = serde_json::to_string(&extra_request)
+                                    .expect("serialisation failed");
+                                serialised += "\n";
+                                if let Err(msg) = cloned_handle.write_all(serialised.as_bytes()) {
+                                    eprintln!("Error sending identification! {}", msg);
+                                    break;
+                                } else {
+                                    // println!("[Server] Sending net path {}", path.clone());
+                                }
+                            }
+                            None => {}
+                        }
+                        recv_msg.clear();
+                        continue;
+                    } else {
+                        has_net = false;
+                    }
+                }
+                MessageType::NewNetworkPath(path) => {
+                    *net_path = Some(path);
+                }
+                MessageType::IdentityConfirmation(_) => {
+                    println!("[Warning] Identity Confirmation Message type is not possible")
+                }
+                MessageType::JobSendData(_) => {}
+                MessageType::NewNetworkData(data) => {
+                    *net_data = Some(data);
                 }
             }
-            MessageType::NewNetworkPath(path) => {
-                *net_path = Some(path);
+            // println!("[Message] {:?}", message);
+            let all_clients = clients.lock().unwrap();
+            for mut client in all_clients.iter() {
+                if let Err(msg) = client.write_all(recv_msg.as_bytes()) {
+                    // eprintln!("Error sending message to client! {}", msg);
+                    continue;
+                }
+                let mut disp_msg = recv_msg.clone();
+                disp_msg.retain(|c| c != '\n'); // remove newline
+                                                // println!("[Sent to {}]: {}", client.peer_addr().unwrap(), disp_msg);
             }
-            MessageType::IdentityConfirmation(_) => {
-                println!("[Warning] Identity Confirmation Message type is not possible")
-            }
-            MessageType::JobSendData(_) => {}
-            MessageType::NewNetworkData(data) => {
-                *net_data = Some(data);
-            }
+            recv_msg.clear();
+            continue;
         }
-        // println!("[Message] {:?}", message);
-        let all_clients = clients.lock().unwrap();
-        for mut client in all_clients.iter() {
-            if let Err(msg) = client.write_all(recv_msg.as_bytes()) {
-                // eprintln!("Error sending message to client! {}", msg);
-                continue;
-            }
-            let mut disp_msg = recv_msg.clone();
-            disp_msg.retain(|c| c != '\n'); // remove newline
-                                            // println!("[Sent to {}]: {}", client.peer_addr().unwrap(), disp_msg);
-        }
-        recv_msg.clear();
-        continue;
     }
-
-    let mut all_clients = clients.lock().unwrap();
-    all_clients
-        .retain(|c| c.peer_addr().is_ok() && c.peer_addr().unwrap() != stream.peer_addr().unwrap());
-    println!(
-        "[Server] Client disconnected: {}",
-        stream.peer_addr().unwrap()
-    );
 }
 
 fn main() {
