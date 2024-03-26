3170e0d54cfb69e201132b545d1f94f074a886fd
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-03-26 21:36:43 +0000

    Update mcts.rs

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,93 +1,129 @@
-use cozy_chess::Board;
-use crossbeam::thread;
-use std::{env, panic, time::Instant};
-use tokio::runtime::Runtime;
-use tz_rust::{
-    boardmanager::BoardStack,
-    executor::{
-        executor_static,
-        Message::{self, StopServer},
-        Packet,
-    },
-    mcts::get_move,
-    mcts_trainer::TypeRequest::NonTrainerSearch,
+use std::time::Instant;
+
+use crate::{
+    boardmanager::BoardStack, dataformat::ZeroEvaluation, executor::Packet, mcts_trainer::Tree,
     settings::SearchSettings,
 };
+use cozy_chess::Move;
 
-fn main() {
-    // test MCTS move outputs
-    panic::set_hook(Box::new(|panic_info| {
-        // print panic information
-        eprintln!("Panic occurred: {:?}", panic_info);
-        // exit the program immediately
-        std::process::exit(1);
-    }));
-    env::set_var("RUST_BACKTRACE", "1");
-    let board = Board::default();
-    // let board = Board::from_fen(
-    //     "6rk/p3p2p/1p2Pp2/2p2P2/2P1nBr1/1P6/P6P/3R1R1K b - - 0 1",
-    //     false,
-    // )
-    // .unwrap(); // black M2
-    // let board = Board::from_fen(
-    //     "r2q1k1r/3bnp2/p1n1pNp1/3pP1Qp/Pp1P4/2PB4/5PPP/R1B2RK1 w - - 1 1",
-    //     false,
-    // )
-    // .unwrap(); // white M2
-    // let board = Board::from_fen("8/6Q1/k7/8/3P4/1P2P1P1/rB3K1P/7q w - - 3 44", false).unwrap();
-    let mut move_list = Vec::new();
-    board.generate_moves(|moves| {
-        // Unpack dense move set into move list
-        move_list.extend(moves);
-        false
-    });
+use flume::Sender;
 
-    let total_moves = move_list.len();
-    // set up executor and sender pairs
+pub async fn get_move(
+    bs: BoardStack,
+    tensor_exe_send: Sender<Packet>,
+    settings: SearchSettings,
+) -> (
+    Move,
+    ZeroEvaluation,
+    Option<Vec<usize>>,
+    ZeroEvaluation,
+    u32,
+) {
+    let sw = Instant::now();
+    // non-generator version of mcts_trainer.rs
 
-    let (tensor_exe_send, tensor_exe_recv) = flume::bounded::<Packet>(1);
-    let (ctrl_sender, ctrl_recv) = flume::bounded::<Message>(1);
-    thread::scope(|s| {
-        s.builder()
-            .name("executor".to_string())
-            .spawn(move |_| {
-                executor_static(
-                    r"C:\Users\andre\RemoteFolder\ZanLing-TrueZero\nets\tz_42.pt".to_string(),
-                    // r"C:\Users\andre\RemoteFolder\ZanLing-TrueZero\chess_16x128_gen3634.pt"
-                    // .to_string(),
-                    tensor_exe_recv,
-                    ctrl_recv,
-                    1,
-                )
+    // most search code is located in mcts_trainer.rs
+
+    let mut tree = Tree::new(bs, settings);
+    if tree.board.is_terminal() {
+        panic!("No valid move!/Board is already game over!");
+    }
+
+    while tree.nodes[0].visits < settings.max_nodes as u32 {
+        let thread_name = std::thread::current()
+            .name()
+            .unwrap_or("unnamed")
+            .to_owned();
+        // println!("step {}", tree.nodes[0].visits);
+        // println!("thread {}, step {}",w thread_name, tree.nodes[0].visits);
+
+        tree.step(&tensor_exe_send, sw, 0).await;
+        // println!("Elapsed time for step: {}ms", sw.elapsed().as_nanos() as f32 / 1e6);
+    }
+    // println!("{}", tree.nodes[0].visits);
+
+    let mut child_visits: Vec<u32> = Vec::new();
+
+    for child in tree.nodes[0].children.clone() {
+        child_visits.push(tree.nodes[child].visits);
+    }
+
+    let all_same = child_visits.iter().all(|&x| x == child_visits[0]);
+
+    let best_move_node = if !all_same {
+        // if visits to nodes are the same eg max_nodes=1
+        tree.nodes[0]
+            .children
+            .clone()
+            .max_by_key(|&n| tree.nodes[n].visits)
+            .expect("Error")
+    } else {
+        tree.nodes[0]
+            .children
+            .clone()
+            .max_by(|a, b| {
+                let a_node = &tree.nodes[*a];
+                let b_node = &tree.nodes[*b];
+                let a_policy = a_node.policy;
+                let b_policy = b_node.policy;
+                a_policy.partial_cmp(&b_policy).unwrap()
             })
-            .unwrap();
+            .expect("Error")
+    };
 
-        println!("Number of legal moves: {}", total_moves);
-        let bs = BoardStack::new(board);
-        let sw = Instant::now();
-        let settings: SearchSettings = SearchSettings {
-            fpu: 0.0,
-            wdl: None,
-            moves_left: None,
-            c_puct: 2.0,
-            max_nodes: 1,
-            alpha: 0.0,
-            eps: 0.0,
-            search_type: NonTrainerSearch,
-            pst: 0.0,
-        };
-        let rt = Runtime::new().unwrap();
-        let (best_move, nn_data, _, _, _) =
-            rt.block_on(async { get_move(bs, tensor_exe_send.clone(), settings.clone()).await });
-        for (mv, score) in move_list.iter().zip(nn_data.policy.iter()) {
-            println!("{:#}, {}", mv, score);
-        }
-        println!("{:#}", best_move);
-        println!("{:?}", nn_data);
-        println!("Elapsed time: {}ms", sw.elapsed().as_nanos() as f32 / 1e6);
-        let nps = settings.max_nodes as f32 / (sw.elapsed().as_nanos() as f32 / 1e9);
-        println!("Nodes per second: {}nps", nps);
-        ctrl_sender.send(StopServer()).unwrap();
-    })
-    .unwrap();
+    let best_move = tree.nodes[best_move_node].mv;
+    let mut total_visits_list = Vec::new();
+    // println!("{:#}", best_move.unwrap());
+    for child in tree.nodes[0].children.clone() {
+        total_visits_list.push(tree.nodes[child].visits);
+    }
+
+    let display_str = tree.display_node(0); // print root node
+                                            // // println!("{}", display_str);
+    let total_visits: u32 = total_visits_list.iter().sum();
+
+    let mut pi: Vec<f32> = Vec::new();
+
+    // println!("{:?}", &total_visits_list);
+
+    for &t in &total_visits_list {
+        let prob = t as f32 / total_visits as f32;
+        pi.push(prob);
+    }
+
+    // // println!("{:?}", &pi);
+    // // println!("{}", best_move.expect("Error").to_string());
+    // // println!("best move: {}", best_move.expect("Error").to_string());
+
+    for child in tree.nodes[0].children.clone() {
+        let display_str = tree.display_node(child);
+        // println!("{}", display_str);
+    }
+    // tree.nodes[0].display_full_tree(&tree);
+
+    let mut all_pol = Vec::new();
+
+    for child in tree.nodes[0].clone().children {
+        all_pol.push(tree.nodes[child].policy);
+    }
+
+    let v_p = ZeroEvaluation {
+        // network evaluation, NOT search/empirical data
+        values: tree.nodes[0].eval_score,
+        policy: all_pol,
+    };
+
+    let search_data = ZeroEvaluation {
+        // search data
+        values: tree.nodes[0].get_q_val(settings),
+        policy: pi,
+    };
+
+    (
+        best_move.expect("Error"),
+        v_p,
+        tree.nodes[0].clone().move_idx,
+        search_data,
+        tree.nodes[0].visits,
+    )
 }
