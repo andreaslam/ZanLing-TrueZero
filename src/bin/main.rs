44b956d1e391b3e84a9e586014023c3665ca9cb6
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-04-07 13:04:30 +0100

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -50,7 +50,7 @@ async fn main() {
         .expect("Failed to send data");
     println!("Connected to server!");
     let num_executors = 2;
-    let batch_size = 131072; // executor batch size
+    let batch_size = 2048; // executor batch size
     let num_generators = num_executors * batch_size * 2;
     let (game_sender, game_receiver) =
         flume::bounded::<CollectorMessage>(num_executors * batch_size);
