dcc2170f9e939c8c0f104eb421ffdbbcc12afb52
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-05-30 11:32:32 +0100

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -48,10 +48,10 @@ fn main() {
         .expect("Failed to send data");
     println!("Connected to server!");
 
-    let mut num_executors = 8;
+    let mut num_executors = 2;
     // num_executors = max(min(tch::Cuda::device_count() as usize, num_executors), 1);
     let batch_size = 8192;
-    let num_generators = num_executors * batch_size * 8;
+    let num_generators = num_executors * batch_size * 32;
 
     let (game_sender, game_receiver) = flume::bounded::<CollectorMessage>(num_generators);
     
