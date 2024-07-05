005a1064f6c8e796dc93c7ed41e9a9f9d23b0a9c
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-07-05 11:58:19 +0100

    Create utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,44 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct TimeStampDebugger {
+    epoch_seconds_start: u128,
+}
+
+impl TimeStampDebugger {
+    pub fn create_debug() -> Self {
+        let now_start = SystemTime::now();
+        let since_epoch = now_start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let epoch_seconds_start = since_epoch.as_nanos();
+        TimeStampDebugger {
+            epoch_seconds_start,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        let now_start = SystemTime::now();
+        let since_epoch = now_start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        self.epoch_seconds_start = since_epoch.as_nanos();
+    }
+
+    pub fn record(&self, message: &str, thread_name: &str) {
+        let now_end = SystemTime::now();
+        let since_epoch_end = now_end
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let epoch_seconds_end = since_epoch_end.as_nanos();
+        let msg = format!(
+            "{} {} {} {}",
+            self.epoch_seconds_start, epoch_seconds_end, thread_name, message
+        );
+        debug_print(&msg);
+    }
+}
+
+pub fn debug_print(msg: &str) {
+    #[cfg(debug_assertions)]
+    println!("{}", msg);
+}
