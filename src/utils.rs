dee7eb03f1316e5ea21ef1a6981d1d0069714bdb
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-07-05 16:36:47 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -17,28 +17,42 @@ impl TimeStampDebugger {
     }
 
     pub fn reset(&mut self) {
-        let now_start = SystemTime::now();
-        let since_epoch = now_start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        self.epoch_seconds_start = since_epoch.as_nanos();
+        if cfg!(debug_assertions) {
+            let now_start = SystemTime::now();
+            let since_epoch = now_start
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            self.epoch_seconds_start = since_epoch.as_nanos();
+        }
     }
 
     pub fn record(&self, message: &str, thread_name: &str) {
-        let now_end = SystemTime::now();
-        let since_epoch_end = now_end
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        let epoch_seconds_end = since_epoch_end.as_nanos();
-        let msg = format!(
-            "{} {} {} {}",
-            self.epoch_seconds_start, epoch_seconds_end, thread_name, message
-        );
-        debug_print(&msg);
+        if cfg!(debug_assertions) {
+            let now_end = SystemTime::now();
+            let since_epoch_end = now_end
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            let epoch_seconds_end = since_epoch_end.as_nanos();
+            let msg = format!(
+                "{} {} {} {}",
+                self.epoch_seconds_start, epoch_seconds_end, thread_name, message
+            );
+            debug_print(&msg);
+        }
     }
 }
 
+macro_rules! debug_only {
+    ($($body:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            $($body)*
+        }
+    };
+}
+
 pub fn debug_print(msg: &str) {
-    #[cfg(debug_assertions)]
-    println!("{}", msg);
+    if cfg!(debug_assertions) {
+        debug_only!(println!("{}", msg));
+    }
 }
