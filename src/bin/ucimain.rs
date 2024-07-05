2a9cf133b9c0091baafe9a26af09ddab533643aa
andreaslam <81166794+andreaslam@users.noreply.github.com>
2024-07-05 11:59:08 +0100

    Update ucimain.rs

--- src/bin/ucimain.rs
+++ src/bin/ucimain.rs
@@ -1,7 +1,9 @@
 use std::env;
+use tch::maybe_init_cuda;
 use tz_rust::uci::run_uci;
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "2");
-    run_uci(r"./tz_6515.pt");
+    maybe_init_cuda();
+    env::set_var("RUST_BACKTRACE", "1");
+    run_uci(r"nets/tz_1391.pt");
 }
